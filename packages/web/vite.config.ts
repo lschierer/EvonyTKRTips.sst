90f876c187da70efbbc8bc2077cab56c4fa7e9f1
Luke Stephen Schierer <luke@schierer.org>
2023-04-19 13:50:33 -0400

    nav bar looks better, multi-page works, nav to the multi page works, so long as I include the final slashes

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    lib: {
-      entry: {
-        myelement: 'src/my-element.ts',
-        ASCompoents: 'src/ASComponents.tsx'
-      },
-      formats: ['es'],
-    },
     rollupOptions: {
       external: /^lit/,
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        generals: path.resolve(__dirname, 'generals/index.html')
+      }
     },
   },
 })
