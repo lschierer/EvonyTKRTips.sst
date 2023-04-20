6ea3d6c4acacc0639e51c8b3cf21b442f24212d8
Luke Stephen Schierer <luke@schierer.org>
2023-04-20 12:38:29 -0400

    this works for pages I have explicetly defined

--- packages/web/src/MarkDown.tsx
+++ packages/web/src/MarkDown.tsx
@@ -1,7 +1,10 @@
-import { LitElement, css, html } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
-import litLogo from './assets/lit.svg'
-import viteLogo from '/vite.svg'
+import { LitElement, html, PropertyValueMap } from 'lit'
+import {until} from 'lit/directives/until.js';
+import { DirectiveResult } from 'lit/directive.js';
+import { customElement, query, state } from 'lit/decorators.js'
+import { resolveMarkdown, MarkdownDirective } from "lit-markdown";
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import { Bucket } from "sst/node/bucket";
 
 
 
@@ -11,119 +14,65 @@ import viteLogo from '/vite.svg'
  * @slot - This element has a slot
  * @csspart button - The button
  */
-@customElement('my-element')
-export class MyElement extends LitElement {
-  /**
-   * Copy for the read the docs hint.
-   */
-  @property()
-  docsHint = 'Click on the Vite and Lit logos to learn more'
+@customElement('mark-down')
+export class MarkDown extends LitElement {
+  @query("textarea")
+  private textarea!: HTMLTextAreaElement;
 
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({ type: Number })
-  count = 0
+  @state()
+  private raw = this.fetchFile();
+
+
+  @state()
+  private myPath = document.location.pathname;
+
+
+  private parseMarkdown(incoming: string) {
+    const value = incoming? incoming.trim() : "";
+    console.log(`value was ${value}`);
+    return resolveMarkdown(value, { includeImages: true, includeCodeBlockClassNames: true });
+  }
+
+  private async fetchFile() {
+    if ((this.myPath === undefined) || (this.myPath === "")) {
+      console.log("no path defined");
+      console.log(`location is ${document.location.pathname}`);
+      if (typeof (document.location.pathname) === "string") {
+        this.myPath = document.location.pathname;
+      }
+    }
+    let filename = "../public/en";
+    if (this.myPath.at(-1) === '/') {
+      filename = filename.concat(this.myPath.concat("index.md"));
+    } else {
+      filename = filename.concat(this.myPath.concat(".md"));
+    }
+    console.log(`going to get ${filename}`);
+    let fileUrl = new URL(filename, import.meta.url).href;
+    fileUrl = fileUrl.replace(/public\//,'');
+    console.log(`resulting url is ${fileUrl}`);
+    let text = await fetch(fileUrl)
+        .then((response) => response.text())
+        .then((text) => {
+          console.log(`text retrieved was ${text}`);
+          return this.parseMarkdown( text);
+          return text;
+        });
+    return text;
+  }
 
   render() {
     return html`
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
+        ${until(this.raw, html`<span>Loading...</span>`)}
       </div>
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
-      </div>
-      <p class="read-the-docs">${this.docsHint}</p>
-    `
+
+    `;
   }
-
-  private _onClick() {
-    this.count++
-  }
-
-  static styles = css`
-    :host {
-      max-width: 1280px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
-    }
-
-    .logo {
-      height: 6em;
-      padding: 1.5em;
-      will-change: filter;
-      transition: filter 300ms;
-    }
-    .logo:hover {
-      filter: drop-shadow(0 0 2em #646cffaa);
-    }
-    .logo.lit:hover {
-      filter: drop-shadow(0 0 2em #325cffaa);
-    }
-
-    .card {
-      padding: 2em;
-    }
-
-    .read-the-docs {
-      color: #888;
-    }
-
-    h1 {
-      font-size: 3.2em;
-      line-height: 1.1;
-    }
-
-    a {
-      font-weight: 500;
-      color: #646cff;
-      text-decoration: inherit;
-    }
-    a:hover {
-      color: #535bf2;
-    }
-
-    button {
-      border-radius: 8px;
-      border: 1px solid transparent;
-      padding: 0.6em 1.2em;
-      font-size: 1em;
-      font-weight: 500;
-      font-family: inherit;
-      background-color: #1a1a1a;
-      cursor: pointer;
-      transition: border-color 0.25s;
-    }
-    button:hover {
-      border-color: #646cff;
-    }
-    button:focus,
-    button:focus-visible {
-      outline: 4px auto -webkit-focus-ring-color;
-    }
-
-    @media (prefers-color-scheme: light) {
-      a:hover {
-        color: #747bff;
-      }
-      button {
-        background-color: #f9f9f9;
-      }
-    }
-  `
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'my-element': MyElement
+    'mark-down': MarkDown
   }
 }
