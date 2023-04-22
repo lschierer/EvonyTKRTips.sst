e785e9aa5fdc46917a7d7db7ab4c960bdec2f56a
Luke Schierer <luke@schierer.org>
2023-04-22 00:10:07 -0400

    half way between converting my markdown parser, but I'm fustrated with vite

---
+++ packages/web/types/TopNav.d.ts
@@ -0,0 +1,8 @@
+import '@spectrum-web-components/theme/sp-theme.js';
+import '@spectrum-web-components/theme/src/themes.js';
+import '@spectrum-web-components/top-nav/sp-top-nav.js';
+import '@spectrum-web-components/top-nav/sp-top-nav-item.js';
+import { TopNav } from '@spectrum-web-components/top-nav';
+export declare class myTopNav extends TopNav {
+    render(): import("lit").TemplateResult<1>;
+}
