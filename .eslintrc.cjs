b8e868f5d933a6c008204654ebdf2ca0fe204651
Luke Stephen Schierer <luke@schierer.org>
2023-04-22 03:25:40 -0400

    eslint conflicts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,25 @@
 module.exports = {
+    parser: '@typescript-eslint/parser',
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
     ],
     plugins: ['@typescript-eslint'],
-    parser: '@typescript-eslint/parser',
+    rules: {
+        'no-unused-vars': 'off',
+        "@typescript-eslint/no-unused-vars": [
+          "error"
+        ],
+        "import/no-unresolved": "off",
+        "import/extensions": [
+            "error",
+            "always",
+            {
+                "ignorePackages": true
+            }
+        ]
+    },
     parserOptions: {
         project: [
             "./tsconfig.json"
