c7fcee0751530edc6807438c6092f35b4ff4a4df
Luke Schierer <luke@schierer.org>
2023-04-23 03:32:41 -0400

    current state, the top nav bar has the right color

--- packages/web/web-dev-server.config.mjs
+++ packages/web/web-dev-server.config.mjs
@@ -10,12 +10,13 @@ export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   nodeResolve: {
     exportConditions: ['browser', 'development'],
   },
-  
+
   /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
   // esbuildTarget: 'auto'
 
   /** Set appIndex to enable SPA routing */
-  appIndex: './index.html',
+  //appIndex: './index.html',
+  rootDir: './dist',
 
   plugins: [
     /** Use Hot Module Replacement by uncommenting. Requires @open-wc/dev-server-hmr plugin */
