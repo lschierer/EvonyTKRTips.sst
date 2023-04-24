54d71d1edc9a45b4a5ef1ef3dc3e1180c34c052a
Luke Stephen Schierer <luke@schierer.org>
2023-04-24 15:39:31 -0400

    I succcessfully used open-wc's configuration

---
+++ packages/web/rollup.config.cjs
@@ -0,0 +1,21 @@
+const merge = require('deepmerge');
+const path = require('path');
+const { createMpaConfig } = require('../../index.js');
+
+const baseConfig = createMpaConfig({
+  developmentMode: true,
+  injectServiceWorker: true,
+  rootDir: __dirname,
+  outputDir: path.join(__dirname, '..', '..', 'dist'),
+});
+
+module.exports = merge(baseConfig, {
+    input: './index.html',
+    plugins: [
+      typescript({
+        experimentalDecorators: true,
+        target: 'esnext'
+      })
+    ],
+  });
+ß
