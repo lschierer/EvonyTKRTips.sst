5ed53419889cb8ad16ca8b42a30c9e2ce5a055a4
Luke Stephen Schierer <luke@schierer.org>
2023-05-01 18:05:44 -0400

    trying Astro again, building my own static generator is a time drain

--- src/components/Spectrum/TopNav.ts
+++ src/components/Spectrum/TopNav.ts
@@ -1,19 +1,23 @@
 import {html, LitElement} from 'lit';
 import {customElement} from 'lit/decorators.js';
+import {styleMap} from 'lit/directives/style-map.js';
 
-import '@spectrum-web-components/theme/sp-theme.js';
-import '@spectrum-web-components/theme/src/themes.js';
+
 import '@spectrum-web-components/top-nav/sp-top-nav.js';
 import '@spectrum-web-components/top-nav/sp-top-nav-item.js';
 import { TopNav } from '@spectrum-web-components/top-nav';
 
+const tagName = 'top-nav';
 
 @customElement('top-nav')
 export class myTopNav extends LitElement {
 
   override render(){
+    const styles = {
+      backgroundColor: 'var(--spectrum-global-color-cyan-600)',
+    }
     return html`
-            <sp-top-nav size="xl" >
+            <sp-top-nav size="xl" client:load style=${styleMap(styles)}>
                 <sp-top-nav-item></sp-top-nav-item>
                 <sp-top-nav-item href="/"><strong>Evony TKR Tips</strong></sp-top-nav-item>
                 <sp-top-nav-item href="/generals/" style="margin-inline-start: auto;">Generals</sp-top-nav-item>
