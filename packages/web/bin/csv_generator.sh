da82bd6f1a414cb0419ed4d57d0382727cb30987
Luke Stephen Schierer <luke@schierer.org>
2023-05-08 15:32:24 -0400

    typo

--- packages/web/bin/csv_generator.sh
+++ packages/web/bin/csv_generator.sh
@@ -5,6 +5,6 @@ cat ground.txt | cut -d / -f 2 | cut -d - -f 1| cat -n | sed -E -e 's/[[:space:]
 cat ground.txt | cut -d / -f 2 | cut -d - -f 2- | cat -n | sed -E -e 's/[[:space:]]+/ /g; s/^[[:space:]]*//; s/[[:space:]]/,/; s/[[:space:]]$//' > g3
 
 join -t, g1 g2> gt
-join -t, gt g2 > ground_attack.csv
+join -t, gt g3 > ground_attack.csv
 rm g1 g2 g3 gt
 
