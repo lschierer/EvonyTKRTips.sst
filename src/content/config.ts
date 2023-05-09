c6f969b1ea7e6ab337e50257dc9a4f0ef2bcfb10
Luke Stephen Schierer <luke@schierer.org>
2023-05-08 23:33:42 -0400

    more towards the second schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,16 @@
 import { defineCollection } from 'astro:content';
-import {docsSchema} from '../schemas/docsSchema';
+import {docsSchema, monsterSchema} from '../schemas/evonySchemas';
 
 const docsCollection = defineCollection({
   schema: docsSchema,
 });
 
+const monsterReports = defineCollection({
+  schema: monsterSchema,
+});
+
 export const collections = {
   docs: docsCollection,
+  killing: monsterSchema,
 };
 
