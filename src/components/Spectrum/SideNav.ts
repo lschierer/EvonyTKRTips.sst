0521379be84c2eb5b196c00fc42de4531c3f0a87
Luke Stephen Schierer <luke@schierer.org>
2023-05-11 16:48:23 -0400

    this gives some visual indication of where you are, despite not auto expanding correctly, nor selecting automatically

--- src/components/Spectrum/SideNav.ts
+++ src/components/Spectrum/SideNav.ts
@@ -3,6 +3,13 @@ import {customElement, property, state} from 'lit/decorators.js';
 import {repeat} from 'lit/directives/repeat.js';
 import {ref,createRef} from 'lit/directives/ref.js';
 import type {TemplateResult} from "@spectrum-web-components/base";
+import '@spectrum-web-components/icon/sp-icon.js';
+import '@spectrum-web-components/icons/sp-icons-medium.js';
+import '@spectrum-web-components/icons-workflow/icons/sp-icon-folder.js';
+import '@spectrum-web-components/icons-workflow/icons/sp-icon-folder-open.js';
+import '@spectrum-web-components/icons-workflow/icons/sp-icon-document-outline.js';
+import '@spectrum-web-components/icons-workflow/icons/sp-icon-file-txt.js';
+import '@spectrum-web-components/icons-workflow/icons/sp-icon-breadcrumb-navigation.js';
 
 import * as d3select from 'd3-selection';
 
@@ -36,19 +43,25 @@ export class SpNavMenu extends LitElement {
 
     navRef:Ref<HTMLInputElement> = createRef();
 
+    private myUrl: URL;
+
     static styles = css`
-        sp-sidenav: {
+        #SideMenu  sp-sidenav {
           flex: 1 1 auto;
           min-height: 1px;
           height: 90vh;
           min-width: 1px;
           width: 100%;
-        }
+          background-color: var(--spectrum-gray-200)
+        };
+        :focus-visible {
+          background-color: var(--spectrum-gray-300)
+        } ;
     `;
 
     constructor(props) {
         super(props);
-
+        this.myUrl = new URL(document.documentURI);
         this.itemsTemplates = [];
         this._entries = new Set();
         this.items_size = 0;
@@ -74,18 +87,20 @@ export class SpNavMenu extends LitElement {
 
     protected renderDir(dirstack: string, level: number) {
 
-        console.log(`going to render ${dirstack}`)
-        console.log(`dirstack is ${dirstack} and level is ${level}`)
+        console.log(`going to render ${dirstack} with level ${level}`)
+
+        console.log(`I am ${this.myUrl}`);
+
         this._entries.add(dirstack);
         let itemTemplates = [];
-        for (let item of this.items) {
+        let temp_items = [].concat(this.items)
+        for (let item of temp_items) {
             console.log(`eval of ${item.slug} looking for ${dirstack}`)
             if (dirstack === item.slug) {
                 continue;
             }
             let s = item.slug;
             let d = dirstack.split('/')
-
 
             if (s.at(0) === '/') {
                 s = s.substring(1);
@@ -95,7 +110,7 @@ export class SpNavMenu extends LitElement {
             let a = s.split('/');
             for (let i = 0; i <= level; i++) {
                 if(d[i] === a[i]) {
-                    console.log(`compared ${d[i]} to ${a[i]} looking for ${dirstack}`)
+                    /*console.log(`compared ${d[i]} to ${a[i]} looking for ${dirstack}`)*/
                     valid = true;
                 } else {
                     valid = false;
@@ -109,7 +124,6 @@ export class SpNavMenu extends LitElement {
                 let l = level;
                 do{
                     b = a.shift();
-                    console.log(`rejecting ${b} out of hand, l is ${l}`)
                     l--;
                 }while(l >= 0);
 
@@ -117,7 +131,8 @@ export class SpNavMenu extends LitElement {
                     b = a.shift();
                     if(!this._entries.has(dirstack + '/' + b)) {
                         itemTemplates.push( html`
-                            <sp-sidenav-item value=${b} label=${b}>
+                            <sp-sidenav-item manage-tab-index value=${b} label=${b}>
+                                <sp-icon-folder slot="icon"></sp-icon-folder>
                                 ${this.renderDir(dirstack + '/' + b, level+1)}
                             </sp-sidenav-item>
                         `);
@@ -128,14 +143,30 @@ export class SpNavMenu extends LitElement {
                     console.log(`u starts as ${u}`)
                     if(!u.endsWith('/')){
                         u = u + '/';
-                        console.log(`u is now ${u}`);
+
                     }
-                    console.log(`about to add dirstack ${dirstack} and slug ${item.slug}`);
+                    console.log(`about to add dirstack ${dirstack} and slug ${item.slug} to u ${u}`);
                     u = u + item.slug;
                     console.log(`u is now ${u}`)
-                    itemTemplates.push( html`
-                        <sp-sidenav-item value=${a} href=${u}>${item.title}</sp-sidenav-item>
-                    `);
+                    if(u.toLocaleString().toLocaleLowerCase().localeCompare(this.myUrl.toString().toLocaleLowerCase()) === 0) {
+                        this.selection = item;
+                        itemTemplates.push( html`
+                            <sp-sidenav-item value=${a} href=${u} selected >
+                                <sp-icon-breadcrumb-navigation slot="icon"></sp-icon-breadcrumb-navigation>
+                                ${item.title}
+                            </sp-sidenav-item>
+                        `);
+                    } else {
+                        itemTemplates.push( html`
+                            <sp-sidenav-item value=${a} href=${u}>
+                                <sp-icon-file-txt slot="icon"></sp-icon-file-txt>
+                                ${item.title}
+                            </sp-sidenav-item>
+                        `);
+                    }
+
+                    let index = temp_items.indexOf(item);
+                    temp_items.splice(index,1);
                 }
 
             }
@@ -152,25 +183,19 @@ export class SpNavMenu extends LitElement {
         if(this.items_size) {
             if(this.items === undefined) {
                 return;
-            } else {
-                console.log(`type of items is ${typeof(this.items)}`)
             }
             this.items.forEach((item) => {
                 if(item.slug.at(0) === '/') {
                     item.slug = item.slug.substring(1);
                 }
                 if(item.slug.includes('/')){
-                    console.log(`ris / in ${item.slug}`)
                     let a = item.slug.split('/');
                     let b = a.shift();
-                    let ni = {
-                        slug: a.join('/'),
-                        title: item.title,
-                        base: item.base
-                    }
+
                     if(!this._entries.has(b)) {
                         itemsTemplates.push(html`
-                        <sp-sidenav-item value=${b} label=${b} >
+                        <sp-sidenav-item manage-tab-index value=${b} label=${b} ${ref(this.expander)} expanded>
+                            <sp-icon-folder-open slot="icon"></sp-icon-folder-open>
                             ${this.renderDir(b, 0)}
                         </sp-sidenav-item>
                     `);
@@ -186,25 +211,31 @@ export class SpNavMenu extends LitElement {
 
     }
 
+    expander(item?: Element) {
+        if(item) {
+            let expanded = item.hasAttribute('expanded');
+            if(expanded){
+                console.log(`${item.value} is expanded`)
+            }
+        }
+
+    }
+
     firstUpdated() {
         const _table = this.navRef.value!;
-        /*this.itemsTemplates = [];
-        if(this.items_size > 0) {
-            let result = this.renderItems();
 
-            if(result !== undefined) {
-                console.log(`result of size ${result?.length}`)
-                this.itemsTemplates = result;
-            }
-        }*/
     }
 
     protected render() {
 
         return html`
-            <sp-sidenav variant="multilevel" defaultValue=${this.selection} id="SideMenu" ${ref(this.renderItems)} >
-                ${this.itemsTemplates}
-            </sp-sidenav>
+            
+            <div id="SideMenu">
+                <sp-sidenav variant="multilevel" manage-tab-index="true" defaultValue=${this.selection} id="SideMenu" ${ref(this.renderItems)} >
+                    ${this.itemsTemplates}
+                </sp-sidenav>    
+            </div>
+            
         `;
     }
 
