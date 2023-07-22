71c3ccd00871f773e3cdc1d26c15c80d45d1f2fb
Luke Schierer <luke@schierer.org>
2023-07-22 19:02:33 -0400

    apollo graphql tutorial

--- stacks/Api.ts
+++ stacks/Api.ts
@@ -1,32 +1,19 @@
-import { use, StackContext, Api as ApiGateway } from "sst/constructs";
+import { use, GraphQLApi, StackContext, Api as ApiGateway } from "sst/constructs";
 import { Database } from "./Database.js";
 
 export function Api({ stack }: StackContext) {
-    const api = new ApiGateway(stack, "api", {
-        defaults: {
-            function: {
-                bind: [use(Database)],
-            },
-        },
-        routes: {
-            "POST /graphql": {
-                type: "graphql",
-                function: {
-                    handler: "packages/functions/src/graphql/graphql.handler",
-                },
-                pothos: {
-                    schema: "packages/functions/src/graphql/schema.ts",
-                    output: "packages/graphql/schema.graphql",
-                    commands: [
-                        "cd packages/graphql && npx @genql/cli --output ./genql --schema ./schema.graphql --esm",
-                    ],
-                },
+    const api = new GraphQLApi(stack, "ApolloApi", {
+        server: {
+            handler: "packages/functions/src/lambda.handler",
+            bundle: {
+                format: "cjs",
             },
         },
     });
 
+    // Show the API endpoint in output
     stack.addOutputs({
-        API: api.url,
+        ApiEndpoint: api.url,
     });
 
     return api;
