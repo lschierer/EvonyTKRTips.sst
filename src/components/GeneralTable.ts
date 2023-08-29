b589c2bb3047af0431a6ad260bfbb269b7fd5a5b
Luke Stephen Schierer <luke@schierer.org>
2023-08-29 01:08:43 -0400

    I think I need to ignore SST's best practices and call electrodb directly from lit

---
+++ src/components/GeneralTable.ts
@@ -0,0 +1,7 @@
+import {LitElement, css, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+@customElement('general-table')
+export class GeneralTable extends LitElement {
+
+}
