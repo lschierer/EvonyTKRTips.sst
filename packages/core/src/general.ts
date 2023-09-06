717a81f51165cc319ff41b726decead230579a2a
Luke Stephen Schierer <luke@schierer.org>
2023-09-06 16:35:27 -0400

    since the table is still empty, I am unsure this works, it errors on a slit somewhere in the library code, but it does deploy and do *something* from the sst console

--- packages/core/src/general.ts
+++ packages/core/src/general.ts
@@ -1,93 +1,116 @@
-import { ulid } from "ulid";
-import { Entity, EntityItem } from "electrodb";
-import { Dynamo } from "./dynamo";
+import {ulid} from "ulid";
+import {Entity, EntityItem} from "electrodb";
+import {Dynamo} from "./dynamo";
 
 export * as General from "./general";
 
 const GeneralEntity = new Entity(
-    {
-        model: {
-            version: "1",
-            entity: "General",
-            service: "evonytips",
-        },
-        attributes: {
-            generalID: {
-                type: "string",
-                required: true,
-                readOnly: true,
-            },
-            name: {
-                type: "string",
-                required: true,
-                readOnly: false,
-            },
-            leadership:{
-                type: "number",
-                required: false,
-                readOnly: false,
-            },
-            attack:{
-                type: "number",
-                required: false,
-                readOnly: false,
-            },
-            defense:{
-                type: "number",
-                required: false,
-                readOnly: false,
-            },
-            politics:{
-                type: "number",
-                required: false,
-                readOnly: false,
-            },
-            maxStars:{
-                type: "number",
-                required: false,
-                readOnly: false,
-            },
-        },
-        indexes: {
-            primary: {
-                pk: {
-                    field: "pk",
-                    composite: ["generalID"],
-                },
-                sk: {
-                    field: "sk",
-                    composite: ["name"],
-                },
-            },
-        },
+  {
+    model: {
+      version: "1",
+      entity: "General",
+      service: "evonytips",
     },
-    Dynamo.Configuration
+    attributes: {
+      generalID: {
+        type: "string",
+        required: true,
+        readOnly: true,
+      },
+      name: {
+        type: "string",
+        required: false,
+        readOnly: false,
+      },
+      leadership: {
+        type: "number",
+        required: false,
+        readOnly: false,
+      },
+      attack: {
+        type: "number",
+        required: false,
+        readOnly: false,
+      },
+      defense: {
+        type: "number",
+        required: false,
+        readOnly: false,
+      },
+      politics: {
+        type: "number",
+        required: false,
+        readOnly: false,
+      },
+      maxStars: {
+        type: "number",
+        required: false,
+        readOnly: false,
+      },
+    },
+    indexes: {
+      byID: {
+        pk: {
+          field: 'pk',
+          composite: ['generalID'],
+        },
+        sk: {
+          field: 'sk',
+          composite: []
+        }
+      },
+      byName: {
+        index: "gsi1pk-gsi1sk-index",
+        pk: {
+          field: 'gsi1pk',
+          composite: ['name'],
+        },
+        sk: {
+          field: "gsi1sk",
+          composite: []
+        }
+      },
+      byStars: {
+        index: "gsi3pk-gsi3sk-index",
+        pk: {
+          field: 'gsi3pk',
+          composite: ['maxStars']
+        },
+        sk: {
+          field: "gsi3sk",
+          composite: []
+        }
+      }
+    }
+  },
+  Dynamo.Configuration
 );
 
 export type GeneralEntityType = EntityItem<typeof GeneralEntity>;
 
-export async function create(name: string, leadership?: number, attack?: number, defense?: number, politics?: number, maxStars?: number){
-    const result = await GeneralEntity.create({
-        generalID: ulid(),
-        name,
-        leadership,
-        attack,
-        defense,
-        politics,
-        maxStars,
-    }).go();
-    return result.data;
+export async function create(name: string, leadership?: number, attack?: number, defense?: number, politics?: number, maxStars?: number) {
+  // no general can have less than 5 stars, if I did not send a number, assume that default.
+  maxStars = maxStars ? maxStars : 5;
+  const result = await GeneralEntity.create({
+    generalID: ulid(),
+    name,
+    leadership,
+    attack,
+    defense,
+    politics,
+    maxStars,
+  }).go();
+  return result.data;
 }
 
-export async function get(generalID: string, name: string) {
-    const result = await GeneralEntity.get({
-        generalID,
-        name
-    }).go();
-    return result.data;
+export async function generalByID(generalID: string) {
+  const result = await GeneralEntity.query.byID({
+    generalID: generalID
+  }).go();
+  return result.data;
 }
 
 export async function list() {
-    const result = await GeneralEntity.scan.go();
-    return result.data;
+  const result = await GeneralEntity.scan.go();
+  return result.data;
 }
