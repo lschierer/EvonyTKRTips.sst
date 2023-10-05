a17ac387fbacd7487b389eed9dac436d52e8063c
Luke Schierer <lschiere@amazon.com>
2023-10-05 14:11:29 -0400

    package updates

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import aws from "astro-sst/lambda";
 import node from '@astrojs/node';
 import lit from "@astrojs/lit";
-import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +10,6 @@ export default defineConfig({
   output: "server",
   adapter: aws(),
   integrations: [
-    markdoc(),
     lit(),
   ],
   vite: {
