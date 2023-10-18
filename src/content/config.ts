07592911aa09b18027fbb16ef5634ac21e423ce8
Luke Stephen Schierer <luke@schierer.org>
2023-10-18 15:50:02 -0400

    catch up the collection definition

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,11 @@
 import {  defineCollection } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
-import {artTreasureSchema,BlazonSetSechma, generalSchema} from '../schemas/evonySchemas.js';
+import {artTreasureSchema, BlazonSetSechma, generalObjectSchema, generalSchema} from '../schemas/evonySchemas.js';
 
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
   i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
   art: defineCollection({type: 'data', schema: artTreasureSchema}),
   blazons: defineCollection({type: 'data', schema: BlazonSetSechma}),
-  generals: defineCollection({type: 'data', schema: generalSchema}),
+  generals: defineCollection({type: 'data', schema: generalObjectSchema}),
 };
