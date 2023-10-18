d4393a3fb0c1138c70db8e5c834cde049fb091a1
Luke Stephen Schierer <luke@schierer.org>
2023-10-18 11:28:17 -0400

    fixed tsc not compiling, and made my score values slightly more realistic

--- src/components/general/score.ts
+++ src/components/general/score.ts
@@ -8,162 +8,209 @@ import {
 export function attack_score(eg: General){
     console.log(`attack_score`)
     let attack = 0;
-    if(eg.score_as !== null && eg.score_as !== undefined) {
-        //const general = eg.general;
-        const general = eg;
+    let totalBuff = 0;
+    if(eg.general.score_as !== null && eg.general.score_as !== undefined) {
+        const general = eg.general;
+        //const general = eg;
         attack = (general.attack + (general.attack_increment * 45 ));
-        general.specialities.map((s) => {
-            s.attribute.map((sa) => {
-                const buff = [sa.buff].flat()
-                buff.map((b) => {
-                    if(b.attribute === 'Attack') {
-                        let multiplier = 1;
-                        if(b.condition !== undefined && b.condition !== null) {
-                            if((b.condition === 'Attacking') ||
-                                (b.condition === 'Marching') ||
-                                (b.condition === 'When Rallying')||
-                                (b.condition === 'leading the army to attack')) {
-                                console.log(`Attacking buff detected`)
-                                switch (general.score_as) {
-                                    case 'Ground':
-                                    case 'Mounted':
-                                    case 'Archers':
-                                        console.log(`score_as matched`)
-                                        multiplier = 0.5;
+        if(general.specialities !== undefined && general.specialities !== null) {
+            general.specialities.map((s) => {
+                s.attribute.map((sa) => {
+                    const buff = [sa.buff].flat()
+                    buff.map((b) => {
+                        if (b.attribute === 'Attack') {
+                            let multiplier = 1;
+                            if (b.condition !== undefined && b.condition !== null) {
+                                switch (b.condition){
+                                    case 'Attacking':
+                                    case 'Marching':
+                                    case 'When Rallying':
+                                    case 'leading the army to attack':
+                                        console.log(`Attacking buff detected`)
+                                        switch (general.score_as) {
+                                            case 'Ground':
+                                            case 'Mounted':
+                                            case 'Archers':
+                                                console.log(`score_as matched`)
+                                                multiplier = 0.5;
+                                                break;
+                                            case 'Siege':
+                                                multiplier = 0.25;
+                                                break;
+                                            default:
+                                                multiplier = 0;
+                                        }
                                         break;
-                                    case 'Siege':
-                                        multiplier = 0.25;
+                                    case 'Reduces Enemy':
+                                        multiplier = 1; // see my explanation
+                                        break;
+                                    case 'Reinforcing':
+                                    case 'In City':
+                                    case 'Defending':
+                                        multiplier = 0.75; //due to reduced utility
+                                        break;
+                                    case 'When The Main Defense General':
+                                    case 'When the City Mayor':
+                                    case 'During SvS':
+                                    case 'When an officer':
+                                    case 'Against Monsters':
+                                    case 'Reduces Monster':
+                                        multiplier = 0;
                                         break;
                                     default:
-                                        multiplier = 0;
+                                        console.log(`no adverb matched`)
+                                        multiplier = 1;
                                 }
-
-                            } else if(b.condition === 'Reduces Enemy') {
-                                multiplier = 1; // see my explanation
-                            } else {
-                                multiplier = 0;
+                            }
+                            if (b.class !== undefined && b.class !== null) {
+                                if (b.class !== 'Ground') {
+                                    console.log(`class was ${b.class}`)
+                                    multiplier = 0;
+                                } else {
+                                    console.log(`leaving multiplier unchanged with ${multiplier}`)
+                                }
+                            }
+                            if (b.value !== undefined && b.value !== null) {
+                                const buff_type = b.value.unit;
+                                if (!buff_type.localeCompare(buffTypeEnum.enum.percentage)) {
+                                   totalBuff = totalBuff + (b.value.number * multiplier);
+                                } else if (!buff_type.localeCompare(buffTypeEnum.enum.flat)) {
+                                    attack = attack + b.value.number;
+                                }
                             }
                         }
-                        if(b.class !== undefined && b.class !== null) {
-                            if (b.class !== 'Ground') {
-                                console.log(`class was ${b.class}`)
-                                multiplier = 0;
-                            }
-                        }
-                        if(b.value !== undefined && b.value !== null) {
-                            const buff_type = b.value.unit;
-                            if(!buff_type.localeCompare(buffTypeEnum.enum.percentage)) {
-                                attack = attack + (attack * (b.value.number * multiplier));
-                            } else if (!buff_type.localeCompare(buffTypeEnum.enum.flat)) {
-                                attack = attack + attack;
-                            }
-                        }
-                    }
+                    })
                 })
             })
-        })
+        }
     }
+    console.log(`total buff is ${totalBuff}`)
+    attack = attack + (attack * totalBuff);
     return attack;
 }
 
 export function defense_score(eg: General){
     let defense = 0;
-    if(eg.score_as !== null && eg.score_as !== undefined) {
-        defense = (eg.defense + (eg.defense_increment * 45))
-        eg.specialities.map((s) => {
-            s.attribute.map((sa) => {
-                const buff = [sa.buff].flat()
-                buff.map((b) => {
-                    if(b.attribute === 'Defense') {
-                        let multiplier = 1;
-                        if(b.condition !== undefined && b.condition !== null) {
-                            if((b.condition === 'Attacking') ||
-                                (b.condition === 'Marching') ||
-                                (b.condition === 'When Rallying')||
-                                (b.condition === 'leading the army to attack')||
-                                (b.condition === 'When an officer')||
-                                (b.condition === 'When the Main Defense General')||
-                                (b.condition === 'When the City Mayor')||
-                                (b.condition === 'Against Monsters')||
-                                (b.condition === 'Reduces Monster')||
-                                (b.condition === 'During SvS')||
-                                (b.condition === 'WHen an officer')) {
-                                multiplier = 0;
-
-                            } else if(b.condition === 'Reduces Enemy') {
-                                multiplier = 1.5; // see my explanation
-                            } else {
-                                multiplier = 1;
+    let totalBuff = 0;
+    if(eg.general.score_as !== null && eg.general.score_as !== undefined) {
+        const general = eg.general;
+        defense = (general.defense + (general.defense_increment * 45))
+        if(general.specialities !== undefined && general.specialities !== null) {
+            general.specialities.map((s) => {
+                s.attribute.map((sa) => {
+                    const buff = [sa.buff].flat()
+                    buff.map((b) => {
+                        if (b.attribute === 'Defense') {
+                            let multiplier = 1;
+                            if (b.condition !== undefined && b.condition !== null) {
+                                switch (b.condition){
+                                    case 'Attacking':
+                                    case 'Marching':
+                                    case 'When Rallying':
+                                    case 'leading the army to attack':
+                                    case 'When The Main Defense General':
+                                    case 'When the City Mayor':
+                                    case 'During SvS':
+                                    case 'When an officer':
+                                    case 'Against Monsters':
+                                    case 'Reduces Monster':
+                                        multiplier = 0;
+                                        break;
+                                    case 'Reinforcing':
+                                    case 'In City':
+                                    case 'Defending':
+                                        multiplier = 1; //not debuffed here because I am looking for defense
+                                    case 'Reduces Enemy':
+                                        multiplier = 1.5; // see my explanation
+                                        break;
+                                    default:
+                                        console.log(`no adverb matched`)
+                                        
+                                    
+                                }
+                            }
+                            if (b.class !== undefined && b.class !== null) {
+                                if (b.class !== 'Ground') {
+                                    multiplier = 0;
+                                }
+                            }
+                            if (b.value !== undefined && b.value !== null) {
+                                const buff_type = b.value.unit;
+                                if (!buff_type.localeCompare(buffTypeEnum.enum.percentage)) {
+                                    totalBuff = totalBuff +  (b.value.number * multiplier);
+                                } else if (!buff_type.localeCompare(buffTypeEnum.enum.flat)) {
+                                    defense = defense + b.value.number;
+                                }
                             }
                         }
-                        if(b.class !== undefined && b.class !== null) {
-                            if (b.class !== 'Ground') {
-                                multiplier = 0;
-                            }
-                        }
-                        if(b.value !== undefined && b.value !== null) {
-                            const buff_type = b.value.unit;
-                            if(!buff_type.localeCompare(buffTypeEnum.enum.percentage)) {
-                                defense = defense + (defense * (b.value.number * multiplier));
-                            } else if (!buff_type.localeCompare(buffTypeEnum.enum.flat)) {
-                                defense = defense + defense;
-                            }
-                        }
-                    }
+                    })
                 })
             })
-        })
+        }
     }
+    defense = defense + (defense * totalBuff);
     return defense;
 }
 
 export function hp_score(eg: General){
     let hp = 0;
-    if(eg.score_as !== null && eg.score_as !== undefined) {
-        hp = (eg.leadership + (eg.leadership_increment * 45))
-        eg.specialities.map((s) => {
-            s.attribute.map((sa) => {
-                const buff = [sa.buff].flat()
-                buff.map((b) => {
-                    if(b.attribute === 'HP') {
-                        let multiplier = 1;
-                        if(b.condition !== undefined && b.condition !== null) {
-                            if((b.condition === 'Attacking') ||
-                                (b.condition === 'Marching') ||
-                                (b.condition === 'When Rallying')||
-                                (b.condition === 'leading the army to attack')||
-                                (b.condition === 'When an officer')||
-                                (b.condition === 'When the Main Defense General')||
-                                (b.condition === 'When the City Mayor')||
-                                (b.condition === 'Against Monsters')||
-                                (b.condition === 'Reduces Monster')||
-                                (b.condition === 'During SvS')||
-                                (b.condition === 'WHen an officer')) {
-                                multiplier = 0;
-
-                            } else if(b.condition === 'Reduces Enemy') {
-                                multiplier = 1.5; // see my explanation
-                            } else {
-                                multiplier = 1;
+    let totalBuff = 0;
+    if(eg.general.score_as !== null && eg.general.score_as !== undefined) {
+        const general = eg.general;
+        hp = (general.leadership + (general.leadership_increment * 45))
+        if(general.specialities !== undefined && general.specialities !== null) {
+            general.specialities.map((s) => {
+                s.attribute.map((sa) => {
+                    const buff = [sa.buff].flat()
+                    buff.map((b) => {
+                        if (b.attribute === 'HP') {
+                            let multiplier = 1;
+                            if (b.condition !== undefined && b.condition !== null) {
+                                console.log(`setting multiplier by condition`)
+                                switch (b.condition){
+                                    case 'Attacking':
+                                    case 'Marching':
+                                    case 'When Rallying':
+                                    case 'leading the army to attack':
+                                    case 'When The Main Defense General':
+                                    case 'When the City Mayor':
+                                    case 'During SvS':
+                                    case 'When an officer':
+                                    case 'Against Monsters':
+                                    case 'Reduces Monster':
+                                        multiplier = 0;
+                                        break;
+                                    case 'Reduces Enemy':
+                                        multiplier = 1.5; // see my explanation
+                                        break;
+                                    case 'Reinforcing':
+                                    case 'In City':
+                                        multiplier = 0.75; // due to reduced utility, partially debuffed
+                                        break;
+                                    case 'Defending':
+                                    default:
+                                        multiplier = 1;
+                                }
+                            }
+                            if (b.class !== undefined && b.class !== null) {
+                                if (b.class !== 'Ground') {
+                                    multiplier = 0;
+                                }
+                            }
+                            if (b.value !== undefined && b.value !== null) {
+                                const buff_type = b.value.unit;
+                                if (!buff_type.localeCompare(buffTypeEnum.enum.percentage)) {
+                                    totalBuff = totalBuff + (b.value.number * multiplier);
+                                } else if (!buff_type.localeCompare(buffTypeEnum.enum.flat)) {
+                                    hp = hp + b.value.number;
+                                }
                             }
                         }
-                        if(b.class !== undefined && b.class !== null) {
-                            if (b.class !== 'Ground') {
-                                multiplier = 0;
-                            }
-                        }
-                        if(b.value !== undefined && b.value !== null) {
-                            const buff_type = b.value.unit;
-                            if(!buff_type.localeCompare(buffTypeEnum.enum.percentage)) {
-                                hp = hp + (hp * (b.value.number * multiplier));
-                            } else if (!buff_type.localeCompare(buffTypeEnum.enum.flat)) {
-                                hp = hp + hp;
-                            }
-                        }
-                    }
+                    })
                 })
             })
-        })
+        }
     }
+    hp = hp + (hp * totalBuff);
     return hp;
 }
