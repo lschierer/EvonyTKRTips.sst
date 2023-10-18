a3ddd84390ad62ee71e005d9de82ca2a53a488f5
Luke Stephen Schierer <luke@schierer.org>
2023-10-17 23:28:08 -0400

    clean up some warnings

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,11 @@
-import { z, defineCollection } from 'astro:content';
+import {  defineCollection } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
-import {artTreasure,BlazonSet, generalSchema} from '../schemas/evonySchemas.js';
-
-
-const legacyReports = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    author: z.string(),
-    sortOrder: z.number().optional(),
-  }),
-});
+import {artTreasureSchema,BlazonSetSechma, generalSchema} from '../schemas/evonySchemas.js';
 
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
   i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
-  art: defineCollection({type: 'data', schema: artTreasure}),
-  blazons: defineCollection({type: 'data', schema: BlazonSet}),
+  art: defineCollection({type: 'data', schema: artTreasureSchema}),
+  blazons: defineCollection({type: 'data', schema: BlazonSetSechma}),
   generals: defineCollection({type: 'data', schema: generalSchema}),
 };
