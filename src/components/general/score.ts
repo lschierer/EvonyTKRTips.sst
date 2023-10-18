3e88cd2a3f3a7e1fd6a45795087b310d8958dc3a
Luke Stephen Schierer <luke@schierer.org>
2023-10-18 18:42:42 -0400

    doing so revealed some flaws, notably I only scored specialities

--- src/components/general/score.ts
+++ src/components/general/score.ts
@@ -1,9 +1,69 @@
 import {
+    type buff,
     buffSchema,
     type buffType, buffTypeEnum,
     buffValueSchema,
     type General
 } from '@schemas/evonySchemas.ts';
+
+function attack_multiplier(b: buff) {
+    let multiplier = 0;
+    if (b.attribute === 'Attack') {
+        multiplier = 1;
+        if (b.condition !== undefined && b.condition !== null) {
+            switch (b.condition){
+                case 'Attacking':
+                case 'Marching':
+                case 'When Rallying':
+                case 'leading the army to attack':
+                    console.log(`Attacking buff detected`)
+                    switch (general.score_as) {
+                        case 'Ground':
+                        case 'Mounted':
+                        case 'Archers':
+                            console.log(`score_as matched`)
+                            multiplier = 0.5;
+                            break;
+                        case 'Siege':
+                            multiplier = 0.25;
+                            break;
+                        default:
+                            multiplier = 0;
+                    }
+                    break;
+                case 'Reduces Enemy':
+                    multiplier = 1; // see my explanation
+                    break;
+                case 'Reinforcing':
+                case 'In City':
+                case 'Defending':
+                    multiplier = 0.75; //due to reduced utility
+                    break;
+                case 'When The Main Defense General':
+                case 'When the City Mayor':
+                case 'During SvS':
+                case 'When an officer':
+                case 'Against Monsters':
+                case 'Reduces Monster':
+                    multiplier = 0;
+                    break;
+                default:
+                    console.log(`no adverb matched`)
+                    multiplier = 1;
+            }
+        }
+        if (b.class !== undefined && b.class !== null) {
+            if (b.class !== 'Ground') {
+                console.log(`class was ${b.class}`)
+                multiplier = 0;
+            } else {
+                console.log(`leaving multiplier unchanged with ${multiplier}`)
+            }
+        }
+        
+    }
+    return multiplier;
+}
 
 export function attack_score(eg: General){
     console.log(`attack_score`)
@@ -14,72 +74,38 @@ export function attack_score(eg: General){
         const general = eg;
         attack = (general.attack + (general.attack_increment * 45 ));
         if(general.specialities !== undefined && general.specialities !== null) {
+            console.log(`speciality buffs`)
             general.specialities.map((s) => {
                 s.attribute.map((sa) => {
                     const buff = [sa.buff].flat()
                     buff.map((b) => {
-                        if (b.attribute === 'Attack') {
-                            let multiplier = 1;
-                            if (b.condition !== undefined && b.condition !== null) {
-                                switch (b.condition){
-                                    case 'Attacking':
-                                    case 'Marching':
-                                    case 'When Rallying':
-                                    case 'leading the army to attack':
-                                        console.log(`Attacking buff detected`)
-                                        switch (general.score_as) {
-                                            case 'Ground':
-                                            case 'Mounted':
-                                            case 'Archers':
-                                                console.log(`score_as matched`)
-                                                multiplier = 0.5;
-                                                break;
-                                            case 'Siege':
-                                                multiplier = 0.25;
-                                                break;
-                                            default:
-                                                multiplier = 0;
-                                        }
-                                        break;
-                                    case 'Reduces Enemy':
-                                        multiplier = 1; // see my explanation
-                                        break;
-                                    case 'Reinforcing':
-                                    case 'In City':
-                                    case 'Defending':
-                                        multiplier = 0.75; //due to reduced utility
-                                        break;
-                                    case 'When The Main Defense General':
-                                    case 'When the City Mayor':
-                                    case 'During SvS':
-                                    case 'When an officer':
-                                    case 'Against Monsters':
-                                    case 'Reduces Monster':
-                                        multiplier = 0;
-                                        break;
-                                    default:
-                                        console.log(`no adverb matched`)
-                                        multiplier = 1;
-                                }
-                            }
-                            if (b.class !== undefined && b.class !== null) {
-                                if (b.class !== 'Ground') {
-                                    console.log(`class was ${b.class}`)
-                                    multiplier = 0;
-                                } else {
-                                    console.log(`leaving multiplier unchanged with ${multiplier}`)
-                                }
-                            }
-                            if (b.value !== undefined && b.value !== null) {
-                                const buff_type = b.value.unit;
-                                if (!buff_type.localeCompare(buffTypeEnum.enum.percentage)) {
-                                   totalBuff = totalBuff + (b.value.number * multiplier);
-                                } else if (!buff_type.localeCompare(buffTypeEnum.enum.flat)) {
-                                    attack = attack + b.value.number;
-                                }
+                        const m = attack_multiplier(buff);
+                        if (b.value !== undefined && b.value !== null) {
+                            const buff_type = b.value.unit;
+                            if (!buff_type.localeCompare(buffTypeEnum.enum.percentage)) {
+                                totalBuff = totalBuff + (b.value.number * m);
+                            } else if (!buff_type.localeCompare(buffTypeEnum.enum.flat)) {
+                                attack = attack + b.value.number;
                             }
                         }
                     })
+                })
+            })
+        }
+        if(general.books !== undefined && general.books !== null) {
+            console.log(`book buffs`)
+            general.books.map((bb) => {
+                const buff = [bb].flat();
+                buff.map((b) => {
+                    const m = attack_multiplier(b);
+                    if (b.value !== undefined && b.value !== null) {
+                        const buff_type = b.value.unit;
+                        if (!buff_type.localeCompare(buffTypeEnum.enum.percentage)) {
+                            totalBuff = totalBuff + (b.value.number * m);
+                        } else if (!buff_type.localeCompare(buffTypeEnum.enum.flat)) {
+                            attack = attack + b.value.number;
+                        }
+                    }
                 })
             })
         }
