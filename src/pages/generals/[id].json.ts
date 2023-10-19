0125f7cc292234744ebb9cbfb8d9528383b54c3d
Luke Stephen Schierer <luke@schierer.org>
2023-10-19 13:03:51 -0400

    two API pages

---
+++ src/pages/generals/[id].json.ts
@@ -0,0 +1,48 @@
+import {
+  type APIRoute,
+  type InferGetStaticParamsType,
+  type InferGetStaticPropsType,
+  type GetStaticPaths
+} from 'astro';
+import { getCollection, getEntry, type CollectionEntry  } from 'astro:content';
+
+import type { HTMLAttributes } from 'astro/types'
+import {z, ZodError} from 'zod'
+
+import {
+  generalSchema,
+  type General,
+  generalObjectSchema,
+  type generalObject
+} from "@schemas/evonySchemas.ts";
+
+
+export const GET: APIRoute = async ({ params, request }) => {
+  let id: string = params.id ? params.id : '';
+  if(id !== '') {
+    if(id.includes('/')){
+      const temp = id.split('/').pop() ;
+      if(temp !== undefined) {
+        id = temp;
+      }
+    }
+    console.log(`id is ${id}`)
+    const entry = await getEntry('generals',id);
+    if(entry !== null && entry !== undefined) {
+      return new Response(
+        JSON.stringify(entry.data)
+      )
+    }
+  }
+  return new Response(JSON.stringify(''))
+}
+
+export async function getStaticPaths() {
+  const generalObjects: CollectionEntry<'generals'>[]  = await getCollection('generals');
+  return generalObjects.map(entry => ({
+    params: {id: entry.id,}, props: { entry },
+  }));
+}
+
+type Params = InferGetStaticParamsType<typeof getStaticPaths>;
+type Props = InferGetStaticPropsType<typeof getStaticPaths>;
