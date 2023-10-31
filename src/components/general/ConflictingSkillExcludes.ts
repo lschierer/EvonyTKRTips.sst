6e90e4cdffdff0839808d4f4131c25e7b6277c38
Luke Schierer <lschiere@amazon.com>
2023-10-31 00:14:11 -0400

    current state

--- src/components/general/ConflictingSkillExcludes.ts
+++ src/components/general/ConflictingSkillExcludes.ts
@@ -3,8 +3,9 @@ import {
   type General,
   generalObjectSchema,
   type generalObject,
+  type standardSkillBookType,
   troopClass,
-  type troopClassType, generalConflictArraySchema, type generalConflictArray,
+  type troopClassType, generalConflicts, type generalConflictsType,
 } from '@schemas/evonySchemas.ts';
 
 import {atom, map, action, computed} from "nanostores";
@@ -18,8 +19,8 @@ type GeneralDictionary = {
 
 const letters = new Set(["a","b","c"]);
 
-const generalConflictCollection = z.array(generalConflictArraySchema);
-export const conflictRecords = atom<generalConflictArray|null>(null);
+const generalConflictCollection = z.array(generalConflicts);
+export const conflictRecords = atom<generalConflictsType|null>(null);
 
 export const conflictingGenerals = computed(conflictRecords, CRs => {
   if(CRs !== undefined && CRs !== null) {
@@ -28,32 +29,37 @@ export const conflictingGenerals = computed(conflictRecords, CRs => {
       let Returnable = new Map<string,Array<string>>();
       for (let i = 0; i < valid.data.length; i++) {
         let o1 = valid.data[i]
-        let valid2 = generalConflictArraySchema.safeParse(o1);
+        let valid2 = generalConflicts.safeParse(o1);
         if (valid2.success) {
-          const data: Record<string, string[]>[] = [valid2.data.conflicts].flat();
-          for (let j = 0; j < data.length; j++) {
-            const o2 = data[j];
-            const keys = Object.keys(o2);
-            for (let k = 0; k < keys.length; k++) {
-              if (keys[k].localeCompare('other')) {
-                for (let l = 0; l < o2[keys[k]].length; l++) {
-                  const o3: string[] = o2[keys[k]];
-                  let n1: string = o3[l];
-                  let confl = new Set<string>();
-                  for (let m = 0; m < keys.length; m++) {
-                    for(let n = 0; n < o2[keys[m]].length; n++) {
-                      const o4: string = o2[keys[m]][n];
-                      if(n1.localeCompare(o4)) {
-                        confl.add(o4);
+          let data: Record<string, string[]> | Record<string, string[]>[] | Record<'books', standardSkillBookType[]> | undefined;
+          if(valid2.data !== undefined && valid2.data.conflicts !== undefined) {
+            let conflicts = [valid2.data.conflicts].flat(1);
+            for (let j = 0; j < conflicts.length; j++) {
+              const o2 = conflicts[j];
+              const keys = Object.keys(o2);
+              for (let k = 0; k < keys.length; k++) {
+                if(keys[k].localeCompare('books')) {
+                  if (keys[k].localeCompare('other')) {
+                    for (let l = 0; l < o2[keys[k]].length; l++) {
+                      const o3: string[] = o2[keys[k]];
+                      let n1: string = o3[l];
+                      let confl = new Set<string>();
+                      for (let m = 0; m < keys.length; m++) {
+                        for(let n = 0; n < o2[keys[m]].length; n++) {
+                          const o4: string = o2[keys[m]][n];
+                          if(n1.localeCompare(o4)) {
+                            confl.add(o4);
+                          }
+                        }
+                        let n2: string = o3[m]
+                        if(n1.localeCompare(n2)) {
+                          confl.add(n2);
+                        }
                       }
-                    }
-                    let n2: string = o3[m]
-                    if(n1.localeCompare(n2)) {
-                      confl.add(n2);
+                      const stringArray: string[] = [...confl];
+                      Returnable.set(n1, stringArray);
                     }
                   }
-                  const stringArray: string[] = [...confl];
-                  Returnable.set(n1, stringArray);
                 }
               }
             }
