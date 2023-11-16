4050a7365eb75925363bb31e1f5e5aa038519aae
Luke Stephen Schierer <luke@schierer.org>
2023-11-16 02:25:37 -0500

    started work simulating a layered version of Derrick Defies Vanilla Battle Simulator

---
+++ src/components/BattleSimulator/siege.ts
@@ -0,0 +1,139 @@
+import { css, html, type CSSResultArray, type PropertyValueMap, type TemplateResult, nothing } from "lit";
+import { property, state } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
+import { createRef, ref, type Ref } from 'lit/directives/ref.js';
+
+import { withStores } from "@nanostores/lit";
+
+const DEBUG = true;
+
+import { SpectrumElement } from '@spectrum-web-components/base';
+
+import {addValue, formValues } from '../formValueStore';
+
+import {Tier, type TierType} from "@schemas/troopsSchemas.ts" 
+import { statusLights, type statusLightsType } from "@schemas/statusLightsSchema.ts";
+
+import * as b from "@schemas/baseSchemas";
+
+
+
+export class EvonySiege extends withStores(SpectrumElement, [formValues]) {
+
+  static hp = {
+    't1':   100,
+    't2':   140,
+    't3':   190,
+    't4':   260,
+    't5':   350,
+    't6':   470,
+    't7':   630,
+    't8':   850,
+    't9':  1150,
+    't10': 1550,
+    't11': 1940,
+    't12': 2425,
+    't13': 2780,
+    't14': 3280,
+    't15': 3780,
+  }
+  static defense = {
+    't1':    50,
+    't2':    70,
+    't3':    90,
+    't4':   120,
+    't5':   160,
+    't6':   220,
+    't7':   300,
+    't8':   410,
+    't9':   550,
+    't10':  740,
+    't11':  930,
+    't12': 1162,
+    't13': 1330,
+    't14': 1560,
+    't15': 1790,
+  }
+  static attack = {
+    't1':   100,
+    't2':   140,
+    't3':   190,
+    't4':   260,
+    't5':   350,
+    't6':   470,
+    't7':   630,
+    't8':   850,
+    't9':  1150,
+    't10': 1550,
+    't11': 1940,
+    't12': 2425,
+    't13': 2780,
+    't14': 3280,
+    't15': 3780,
+  }
+  static ranges = {
+    [Tier.enum.t1]:  1400,
+    [Tier.enum.t2]:  1400,
+    [Tier.enum.t3]:  1400,
+    [Tier.enum.t4]:  1400,
+    [Tier.enum.t5]:  1556,
+    [Tier.enum.t6]:  1556,
+    [Tier.enum.t7]:  1556,
+    [Tier.enum.t8]:  1556,
+    [Tier.enum.t9]:  1771,
+    [Tier.enum.t10]: 1771,
+    [Tier.enum.t11]: 1867,
+    [Tier.enum.t12]: 1867,
+    [Tier.enum.t13]: 2178,
+    [Tier.enum.t14]: 2178,
+    [Tier.enum.t15]: 2178,
+  }
+
+  static speed = 75;
+
+  static modifiersByClass = {
+    [b.ClassEnum.enum.Siege]: 0.5,
+    [b.ClassEnum.enum.Archers]: 0.4,
+    [b.ClassEnum.enum.Mounted]: 0.3,
+    [b.ClassEnum.enum.Ground]: 0.35,
+  }
+
+  static modifiersByClass_T11_T15 = {
+    [b.ClassEnum.enum.Siege]: 0.6,
+    [b.ClassEnum.enum.Archers]: 0.4,
+    [b.ClassEnum.enum.Mounted]: 0.3,
+    [b.ClassEnum.enum.Ground]: 0.35,
+  }
+
+  @property({type: Number})
+  public count: number = 0;
+
+  @property({type: String})
+  public tier: TierType = Tier.enum.t1;
+
+  @state()
+  accessor range: number = 0;
+
+  public override willUpdate(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
+    super.willUpdate(_changedProperties) ;
+
+    if(this.count > 0) {
+      this.range = EvonySiege.ranges[this.tier as keyof typeof EvonySiege.ranges]
+    }
+  }
+
+  constructor() {
+    super()
+    formValues.subscribe((fv) => {
+      if(fv !== null && fv !== undefined) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+  }
+
+}
+if (!customElements.get('evony-siege')) {
+  customElements.define('evony-siege', EvonySiege)
+}
---
+++ src/schemas/troopsSchemas.ts
@@ -0,0 +1,21 @@
+import {z} from 'zod';
+
+export const Tier = z.enum([
+  't1',
+  't2',
+  't3',
+  't4',
+  't5',
+  't6',
+  't7',
+  't8',
+  't9',
+  't10',
+  't11',
+  't12',
+  't13',
+  't14',
+  't15',
+  't16',
+]);
+export type TierType = z.infer<typeof Tier>;
