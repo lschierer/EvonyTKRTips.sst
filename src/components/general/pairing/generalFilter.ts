b08f5e6ac9ad60e99d01e50e5fbbdccbf6a5f3ea
Luke Schierer <luke@schierer.org>
2023-11-28 04:07:23 -0500

    I am crashing, but I need to stop for the night

--- src/components/general/pairing/generalFilter.ts
+++ src/components/general/pairing/generalFilter.ts
@@ -1,11 +1,10 @@
 import {html, css, nothing, type CSSResultArray, type PropertyValues, type PropertyValueMap} from "lit";
 import {customElement, property, state} from 'lit/decorators.js';
-import { ifDefined } from 'lit/directives/if-defined.js';
+import {ref} from 'lit/directives/ref.js';
 
 import {boolean, z} from 'zod';
 
 import { action, onMount, task} from "nanostores";
-import { persistentAtom } from '@nanostores/persistent'
 import { logger } from '@nanostores/logger'
 import {withStores} from "@nanostores/lit";
 
@@ -18,6 +17,7 @@ import '@spectrum-web-components/menu/sp-menu-item.js';
 import '@spectrum-web-components/menu/sp-menu-divider.js';
 import '@spectrum-web-components/overlay/overlay-trigger.js';
 import '@spectrum-web-components/picker/sp-picker.js';
+import '@spectrum-web-components/picker-button/sp-picker-button.js';
 import '@spectrum-web-components/popover/sp-popover.js';
 import '@spectrum-web-components/radio/sp-radio.js';
 import '@spectrum-web-components/radio/sp-radio-group.js';
@@ -29,8 +29,9 @@ import {
   MenuItem,
   MenuDivider
 } from '@spectrum-web-components/menu';
+import { Button, ClearButton, CloseButton } from '@spectrum-web-components/button';
 
-const DEBUG = true;
+const DEBUG = false;
 
 import * as b from '@schemas/baseSchemas.ts'
 
@@ -40,59 +41,11 @@ import {
     GeneralElementSchema,
 } from "@schemas/generalsSchema";
 
-import { allGenerals } from "./generals";
+import { type GeneralToggle, allGenerals, filteredPrimaries, selectedPrimaries, selectedSecondaries, togglePrimary, toggleSecondary, resetPrimary, resetSecondary } from "./generals";
 
-export interface GeneralToggle {
-  [key: string]: boolean;
-}
-
-export const selectedPrimaries = persistentAtom<GeneralToggle[]>('evonyTKRTipsPrimaries', [], {
-  listen: false,
-  encode: JSON.stringify,
-  decode: JSON.parse,
-})
-
-export const selectedSecondaries = persistentAtom<Record<string,boolean>[]>('secondaries', [], {
-    encode: JSON.stringify,
-    decode: JSON.parse,
-})
-
-let destroy = logger({
-  'selectedPrimaries': selectedPrimaries,
-  'selectedSecondaries': selectedSecondaries,
-})
-
-export const togglePrimary = action(selectedPrimaries, 'toggle', (store, general: string, enabled: boolean) => {
-  const data = store.get();
-  const keys = new Set(data.map((so) => Object.keys(so)[0]));
-  
-  if(DEBUG) {console.log(`set of ${keys.size}`)}
-  const trueVersion = {[general]: true};
-  const falseVersion = {[general]: false};
-  if(DEBUG) {console.log(`true is ${JSON.stringify(trueVersion)} and false is ${JSON.stringify(falseVersion)}`)}
-  const index = data.findIndex((element) => {
-    const k = Object.keys(element)[0];
-    return (!(k as string).localeCompare(general))
-  })
-  data[index] = {[general]: enabled}
-  store.set(data);
-})
-
-const primaryEnabled = (mykey: string) => {
-  const values = new Map<string, number|string|boolean>();
-  const storedValues = selectedPrimaries.get();
-  if(storedValues !== null && storedValues !== undefined) {
-    storedValues.forEach((val) => {
-      const k = Object.keys(val)[0];
-      values.set(k,val[k]);
-    })
-    return values.get(mykey);
-  }
-  return false;
-}
 
 @customElement('general-filter')
-export class GeneralFilter extends withStores(SpectrumElement, [allGenerals, selectedPrimaries, selectedSecondaries]) { 
+export class GeneralFilter extends withStores(SpectrumElement, [allGenerals, filteredPrimaries, selectedPrimaries, selectedSecondaries]) { 
 
   constructor() {
     super();
@@ -105,17 +58,11 @@ export class GeneralFilter extends withStores(SpectrumElement, [allGenerals, sel
 
     allGenerals.subscribe(ag => {
       if(DEBUG) {console.log(`ag subscribe in general-filter`) }
-      if(ag !== null && ag !== undefined) {
+      /*if(ag !== null && ag !== undefined) {
         const valid = GeneralArray.safeParse(ag);
-        if(valid.success) {
-          const spv = selectedPrimaries.get();
+        const spv = selectedPrimaries.get();
+        if(valid.success && (spv !== null && spv !== undefined)) {
           const ssv = selectedSecondaries.get();
-          const spKeys = new Set(spv.map((spe) => {
-            if(spe !== null && spe !== undefined) {
-              const k = Object.keys(spe)[0];
-              return k;
-            }
-          }))
           const ssKeys = new Set(ssv.map((sse) => {
             if(sse !== null && sse !== undefined) {
               const k = Object.keys(sse)[0];
@@ -125,10 +72,10 @@ export class GeneralFilter extends withStores(SpectrumElement, [allGenerals, sel
           for (let i in valid.data) {
             const one = valid.data[i];
             const name = one.general.name;
-            if(!spKeys.has(name)) {
-              const toAdd = {[name]: true};
-              spv.push(toAdd);
+            if(!spv.has(one)){
+              spv.set(one,true);
               selectedPrimaries.set(spv);
+              this.requestUpdate();
             }
             if(!ssKeys.has(name)) {
               const toAdd = {[name]: true};
@@ -137,7 +84,7 @@ export class GeneralFilter extends withStores(SpectrumElement, [allGenerals, sel
             }
           }
         }
-      }
+      }*/
     })
 
   }
@@ -161,21 +108,39 @@ export class GeneralFilter extends withStores(SpectrumElement, [allGenerals, sel
       super.willUpdate(_changedProperties);
   }
 
+  
   public static override get styles(): CSSResultArray {
     const localstyle = css`
     
       .outside {
         display: flex;
+        flex-direction: column;
         align-items: center;
-        justify-content: space-evenly;
-        padding: 0.5rem;
+        justify-content: space-around;
         border-top: 1px solid var(--spectrum-cyan-600);
         border-right: 1px solid var(--spectrum-cyan-600);
         border-left: 0px;
         border-bottom: 0px;
         height: 100%;
-      }
 
+        & .primary {
+          width: 100%;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: space-evenly;
+        }
+
+        & .secondary {
+          width: 100%;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: space-evenly;
+        }
+
+      }
+      
     `
     if (super.styles !== null && super.styles !== undefined) {
       return [super.styles, localstyle];
@@ -184,8 +149,45 @@ export class GeneralFilter extends withStores(SpectrumElement, [allGenerals, sel
 
   public togglePrimarySelection(e: CustomEvent) {
 
+    const values = (e.target as Menu).value.split(',');
+    if(values.includes("all")){
+      console.log(`I should reset`)
+      return;
+    }
+    const existing = selectedPrimaries.get();
+    const eKeys = new Set();
+    values.forEach((v) => {
+      const g = allGenerals.get()?.filter((value) => {
+        return (!value.general.name.localeCompare(v))
+      }).pop();
+      if(g !== undefined) {
+        if(existing.has(g)){
+          if(existing.get(g) !== true) {
+            togglePrimary(g,true);
+            this.requestUpdate();
+          }
+        } else {
+          togglePrimary(g,true);
+          this.requestUpdate();
+        }
+      } else {
+        console.error(`I cannot find a general with ${v}`)
+      }
+    })
+    allGenerals.get()?.forEach((gen) => {
+      if(gen !== null && gen !== undefined) {
+        if(!values.includes(gen.general.name)){
+          togglePrimary(gen,false);
+          this.requestUpdate();
+        }
+      }
+    })
+  }
+
+  public toggleSecondarySelection(e: CustomEvent) {
+
     const values = new Set((e.target as Menu).value.split(','));
-    const existing = new Set(selectedPrimaries.get());
+    const existing = new Set(selectedSecondaries.get());
     const eKeys = new Set();
     existing.forEach((ev) => {
       if(ev !== null && ev !== undefined) {
@@ -194,25 +196,42 @@ export class GeneralFilter extends withStores(SpectrumElement, [allGenerals, sel
         const trueVersion = {[k]: true};
         const falseVersion = {[k]: false};
         if(values.has(k)){
-          togglePrimary(k,true);
+          toggleSecondary(k,true);
         } else {
-          togglePrimary(k,false);
+          toggleSecondary(k,false);
         }
       }
     })
   }
 
+  primaryCickListener(e?: Element) {
+    if(e !== null && e !== undefined) {
+      (e as Button).addEventListener('click', () => {resetPrimary()});
+    }
+  }
+
+  secondaryCickListener(e?: Element) {
+    if(e !== null && e !== undefined) {
+      (e as Button).addEventListener('click', () => {resetSecondary()});
+    }
+  }
+
   render() {
     let spt = html``;
     const primaries = selectedPrimaries.get();
-    if(DEBUG) {console.log(`render SP has ${primaries.length} entries`)}
+    const generals = allGenerals.get();
+
     const secondaries = selectedSecondaries.get();
-    if(primaries !== null && primaries !== undefined) {
-      for(let i = 0; i < primaries.length; i++) {
-        if(primaries[i] !== null && primaries[i] !== undefined) {
-          const v = primaries[i];
-          const k = Object.keys(v)[0];
-          const e = v[k]; 
+    if(primaries !== null && primaries !== undefined && generals !== undefined && generals !== null) {
+      for(let i = 0; i < generals?.length; i++) {
+        if(generals[i] !== null && generals[i] !== undefined) {
+          const v = generals[i];
+          const k = v.general.name;
+          let e: boolean = true;
+          if(primaries.has(v)) {
+            const tResult = primaries.get(v);
+            e = (tResult !== undefined) ? tResult : true;
+          }
           if (DEBUG) {console.log(`${k} set to ${e}`) }
           if(k !== null && k !== undefined) {
             spt = html`${spt}
@@ -225,17 +244,55 @@ export class GeneralFilter extends withStores(SpectrumElement, [allGenerals, sel
       spt = html`
       <sp-popover slot="click-content" open style="position: relative">
         <sp-menu label="Primary Generals" selects="multiple" @change=${this.togglePrimarySelection}>
+          <sp-menu-item value="all">All</sp-menu-item>
+          <sp-menu-divider></sp-menu-divider>
           ${spt}
         </sp-menu>
       </sp-popover>
       `
 
+      let sst = html``;
+      if(secondaries !== null && secondaries !== undefined) {
+        for(let i = 0; i < secondaries.length; i++) {
+          if(secondaries[i] !== null && secondaries[i] !== undefined) {
+            const v = secondaries[i];
+            const k = Object.keys(v)[0];
+            const e = v[k]; 
+            if (DEBUG) {console.log(`${k} set to ${e}`) }
+            if(k !== null && k !== undefined) {
+              sst = html`${sst}
+                <sp-menu-item value=${k} selected=${(e === true)? true : nothing}>${k}</sp-menu-item>
+              `
+            }
+          }
+          
+        }
+      }
+      sst = html`
+      <sp-popover slot="click-content" open style="position: relative">
+        <sp-menu label="Secondary Generals" selects="multiple" @change=${this.togglePrimarySelection}>
+          
+          ${sst}
+        </sp-menu>
+      </sp-popover>
+      `
+
       return html`
-      <div class="outside">
-        <overlay-trigger id="trigger" placement="bottom" offset="6">
-          <sp-action-button slot="trigger" toggles type='button' >Primary Generals</sp-action-button>
-          ${spt}
-        </overlay-trigger>
+      <div class="not-content outside">
+        <div class="not-content primary">
+          <overlay-trigger id="trigger" placement="bottom" offset="6">
+            <sp-picker-button size="m" slot="trigger" type='button' ><span slot="label">Primary Generals</span></sp-picker-button>
+            ${spt}
+          </overlay-trigger>
+          <sp-button treatment="outline" ${ref(this.primaryCickListener)}variant="primary" >Reset</sp-button>
+        </div>
+        <div class="not-content secondary">
+          <overlay-trigger id="trigger" placement="bottom" offset="6">
+            <sp-picker-button size="m" slot="trigger" type='button' ><span slot="label">Secondary Generals</span></sp-picker-button>
+            ${spt}
+          </overlay-trigger>
+          <sp-button treatment="outline" ${ref(this.secondaryCickListener)}variant="primary" >Reset</sp-button>
+        </div>
       </div>
       `
     }
--- src/components/general/pairing/generals.ts
+++ src/components/general/pairing/generals.ts
@@ -1,13 +1,14 @@
 import { atom, map, action, computed } from "nanostores";
+import { persistentAtom, persistentMap } from '@nanostores/persistent'
 import { logger } from '@nanostores/logger'
 
-import { z } from "zod";
+import { z, type ZodError } from "zod";
 
-const DEBUG = false;
+const DEBUG = true;
 
 import * as b from "@schemas/baseSchemas.ts";
 
-import {type generalInvestment, } from './selectionStore'
+import { type generalInvestment, } from './selectionStore'
 
 import {
   GeneralClass,
@@ -26,24 +27,88 @@ import { conflictingGenerals } from "./ConflictingSkillExcludes.ts"
 
 import { typeAndUseMap, primaryInvestmentMap, secondaryInvestmentMap } from "./selectionStore.ts";
 
-import { buffAdverbs, buff} from './buff'
+import { buffAdverbs, buff } from './buff';
 
+export interface GeneralToggle {
+  [key: string]: boolean;
+}
 
 export const allGenerals = atom<GeneralArrayType | null>(null);
 
-export const generalPairs = computed([allGenerals, primaryInvestmentMap, secondaryInvestmentMap, typeAndUseMap, conflictingGenerals],
-   (g, pim, sim, tam, c) => {
+
+export const selectedPrimaries = persistentAtom<WeakMap<GeneralElement,boolean>>('evonyTKRTipsPrimaries', new WeakMap(), {
+  listen: false,
+  encode: JSON.stringify,
+  decode: JSON.parse,
+})
+
+export const selectedSecondaries = persistentAtom<Record<string,boolean>[]>('secondaries', [], {
+    encode: JSON.stringify,
+    decode: JSON.parse,
+})
+
+export const filteredPrimaries = computed([allGenerals,selectedPrimaries], (ag, sp) => {
+  const returnable = new Set<GeneralElement>();
+  const agv = GeneralArray.safeParse(ag);
+  
+  if(agv.success && (sp !== undefined && sp !== null)) {
+    
+    for(let i in agv.data) {
+      const one = agv.data[i];
+      if(sp.has(one) && (sp.get(one) === true)) {
+        returnable.add(one);
+      }
+    }
+  }
+  return [...returnable];
+})
+
+export const filteredSecondaries = computed([allGenerals, selectedSecondaries], (ag, ss) => {
+  const returnable = new Set<GeneralElement>();
+  const agv = GeneralArray.safeParse(ag);
+  if(agv.success && ss.length > 0) {
+    const ssS = new Set(ss.map((sse) => {
+      const k = Object.keys(sse)[0]
+      if(sse[k] === true) {
+        return k;
+      }
+    }));
+    
+    for(let i in agv.data) {
+      const one = agv.data[i];
+      if(ssS.has(one.general.name)) {
+        returnable.add(one);
+      }
+    }
+  }
+  return [...returnable];
+})
+
+export const generalPairs = computed([allGenerals, filteredPrimaries, filteredSecondaries, primaryInvestmentMap, secondaryInvestmentMap, typeAndUseMap, conflictingGenerals],
+  (g,fp, fs,  pim, sim, tam, c) => {
     const returnable = new Map<string, GeneralPairType[]>();
     const type = tam.type;
 
-    
+
     if (DEBUG) { console.log(`generals.ts generalPairs start`) }
     if (g !== null && g !== undefined) {
       if (c !== null && c !== undefined) {
+        const fpv:{ success: true; data: GeneralArrayType } | { success: false; error: ZodError; } = GeneralArray.safeParse(fp);
+        const fsv = GeneralArray.safeParse(fs);
+        if(!fpv.success){
+          console.error(`filtered Generals invalid`)
+        } else {
+          if(DEBUG) {console.log(`fpv has ${fpv.data.length} entries`)}
+        }
+        if(!fsv.success){
+          console.error(`filtered Generals invalid`)
+        } else {
+          if(DEBUG) {console.log(`fsv has ${fsv.data.length} entries`)}
+        }
         const valid = GeneralArray.safeParse(g);
-        if (valid.success) {
-          for (let i in valid.data) {
-            const one = valid.data[i];
+        if (valid.success && fpv.success && fsv.success) {
+          for (let i in fpv.data) {
+            const one = fpv.data[i];
             if (one.general.score_as !== null && one.general.score_as !== undefined) {
               if (one.general.score_as !== null && one.general.score_as !== undefined) {
                 if (type !== null && type !== undefined && type !== b.ClassEnum.enum.all) {
@@ -54,11 +119,11 @@ export const generalPairs = computed([allGenerals, primaryInvestmentMap, seconda
                 }
               }
             }
-            
+
             const conflicts = c.get(one.general.name);
             const pairs = new Set<GeneralPairType>
-            for (let j in valid.data) {
-              const two = valid.data[j];
+            for (let j in fsv.data) {
+              const two = fsv.data[j];
               if (one.general.name.localeCompare(two.general.name, undefined, { sensitivity: 'base' })) {
                 if (conflicts !== null && conflicts !== undefined && conflicts.length >= 1) {
                   if (!conflicts.includes(two.general.name)) {
@@ -73,7 +138,7 @@ export const generalPairs = computed([allGenerals, primaryInvestmentMap, seconda
                     pairs.add({ primary: one.general, secondary: two.general })
                   }
                 } else {
-                  if (DEBUG) {console.log(`general ${one.general.name} has no conflicts`)}
+                  if (DEBUG) { console.log(`general ${one.general.name} has no conflicts`) }
                   if (type !== null && type !== undefined && type !== b.ClassEnum.enum.all) {
                     if (two.general.score_as !== null && two.general.score_as !== undefined) {
                       if (two.general.score_as !== type) {
@@ -97,17 +162,62 @@ export const generalPairs = computed([allGenerals, primaryInvestmentMap, seconda
         }
       }
     } else {
-      if (DEBUG) {console.log(`generalPairs computed before generals present`)}
+      if (DEBUG) { console.log(`generalPairs computed before generals present`) }
     }
     if (returnable.size >= 1) {
       return returnable;
     } else {
       return null;
     }
-  })
+  }
+)
 
+
+export const togglePrimary = action(selectedPrimaries, 'toggle', (store, general: GeneralElement, enabled: boolean) => {
+  const data = store.get();
+  data.set(general,enabled);
+  store.set(data);
+})
+
+export const toggleSecondary = action(selectedSecondaries, 'toggle', (store, general: string, enabled: boolean) => {
+  const data = store.get();
+  const keys = new Set(data.map((so) => Object.keys(so)[0]));
+  
+  if(DEBUG) {console.log(`set of ${keys.size}`)}
+  const trueVersion = {[general]: true};
+  const falseVersion = {[general]: false};
+  if(DEBUG) {console.log(`true is ${JSON.stringify(trueVersion)} and false is ${JSON.stringify(falseVersion)}`)}
+  const index = data.findIndex((element) => {
+    const k = Object.keys(element)[0];
+    return (!(k as string).localeCompare(general))
+  })
+  data[index] = {[general]: enabled}
+  store.set(data);
+})
+
+export const resetPrimary = action(selectedPrimaries, 'reset', (store) => {
+  let data = store.get();
+  const generals = allGenerals.get();
+  if(generals !== null && generals !== undefined) {
+    generals.forEach((value,key) => {
+      data.set(value,true);
+    })
+    store.set(data);
+  }
+})
+
+export const resetSecondary = action(selectedSecondaries, 'reset', (store) => {
+  let data = store.get();
+  const keys = data.map((so) => Object.keys(so)[0]);
+  data = data.map((value, index) => {
+    return {[keys[index] as string]: true};
+  })
+  store.set(data);
+})
 
 let destroy = logger({
   'AllGenerals': allGenerals,
   'GeneralPairs': generalPairs,
+  'selectedPrimaries': selectedPrimaries,
+  'selectedSecondaries': selectedSecondaries,
 })
