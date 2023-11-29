846fd730be444c9fc42c4ae318921e424d675f5e
Luke Stephen Schierer <luke@schierer.org>
2023-11-29 02:38:56 -0500

    these are effectively useless, execept as reference

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -2,3 +2,23 @@ export function isEmpty(obj: Record<string, any>): boolean {
     return Object.keys(obj).length === 0 && obj.constructor === Object;
 }
   
+type T = /*unresolved*/ any
+
+export function setDifference(setA: Set<T>, setB: Set<T>) {
+    const _difference = new Set([...setA]);
+    for (const elem of setB) {
+      _difference.delete(elem);
+    }
+    return _difference;
+  }
+  
+export function arrayDifference(a: Array<T>, b: Array<T>) {
+    const _difference = new Array();
+    for(let i = 0; i < a.length; i++) {
+        const v = a[i];
+        if(!b.includes(v)) {
+            _difference.push(v)
+        }
+    }
+    return _difference;
+}   
