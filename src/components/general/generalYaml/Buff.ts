2fb4b78cc2a5eb703e2070a419ad741d2719fb4b
Luke Stephen Schierer <luke@schierer.org>
2024-04-15 16:53:09 -0400

    fix some debugging

--- src/components/general/generalYaml/Buff.ts
+++ src/components/general/generalYaml/Buff.ts
@@ -3,7 +3,7 @@ import {customElement, property, state} from 'lit/decorators.js';
 import {ref, createRef,  type Ref} from 'lit/directives/ref.js';
 import { StoreController } from "@nanostores/lit";
 
-const DEBUG = true;
+const DEBUG = false;
 
 
 import { SpectrumElement } from '@spectrum-web-components/base';
