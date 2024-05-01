40a95e440e27eda20c303c92a08e7e8fb30aac25
Luke Stephen Schierer <luke@schierer.org>
2024-05-01 10:49:50 -0400

    linting works

--- eslint.config.js
+++ eslint.config.js
@@ -9,11 +9,22 @@ export default tseslint.config(
   {
     ignores: [
       "astro.config.ts",
+      "dist/**/*.js",
       "eslint.config.js",
+      "node_modules/**/*.js",
       "src/env.d.ts",
       "src/sst-env.d.ts",
       "src/**/*.js",
       "sst.config.ts",
+      "astro.config.ts",
+      "eslint.config.js",
+      "node_modules",
+      "src/env.d.ts",
+      "src/sst-env.d.ts",
+      "src/**/*.js",
+      "sst.config.ts",
+      ".sst/**/*.js",
+      ".sst/**/*.ts"
     ],
   },
   eslint.configs.recommended,
