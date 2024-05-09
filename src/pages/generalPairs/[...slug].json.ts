2e102723b7a4b7b92690346e4e0d6d4672aa846c
Luke Stephen Schierer <luke@schierer.org>
2024-05-09 19:05:29 -0400

    this page displays the pairs of generals correctly as best I can tell, though I have not fully spot checked the conflict resolution

--- src/pages/generalPairs/[...slug].json.ts
+++ src/pages/generalPairs/[...slug].json.ts
@@ -9,12 +9,14 @@ import {
   type InferGetStaticParamsType,
   type InferGetStaticPropsType,
 } from "astro";
-import { getCollection, type CollectionEntry } from "astro:content";
+import { getCollection, getEntry, type CollectionEntry } from "astro:content";
 
 import {
   ConflictDatum,
   type ConflictDatumType,
   generalConflicts,
+  GeneralPair,
+  type GeneralPairType,
   generalSpecialists,
 } from "@schemas/index";
 
@@ -24,6 +26,13 @@ export const GET: APIRoute = async ({ params, request }) => {
   if (DEBUG) console.log(`slug is ${params.slug}`);
   if (DEBUG) console.log(`id is ${id}`);
   if (DEBUG) console.log(`gc is ${gc}`);
+
+  const g1Entry = await getEntry('generals', `${id}`);
+  if(g1Entry === undefined || g1Entry === null) {
+    return new Response(JSON.stringify(`unknown general ${id}`));
+  }
+  const g1 = g1Entry.data.general;
+
   if (id !== undefined && id !== null) {
     const collectionArray: CollectionEntry<"generalConflictData">[] =
       await getCollection("generalConflictData", ({ data }) => {
@@ -33,7 +42,7 @@ export const GET: APIRoute = async ({ params, request }) => {
         }
       });
     let conflictData: ConflictDatumType[] | null = null;
-
+    
     if (collectionArray !== null && collectionArray !== undefined) {
       conflictData = collectionArray
         .filter((ca) => {
@@ -48,7 +57,7 @@ export const GET: APIRoute = async ({ params, request }) => {
         .map((gci) => {
           return gci.data;
         });
-
+      
       const generalObjects: CollectionEntry<"generals">[] = (
         await getCollection("generals")
       ).filter((gi) => {
@@ -66,10 +75,10 @@ export const GET: APIRoute = async ({ params, request }) => {
           }
           if (conflictData !== null) {
             for (const datum of conflictData) {
-              const conflicts = [
-                ...Object.values(datum.conflicts)[0],
-                ...Object.values(datum.conflicts)[1],
-              ];
+              const conflicts = [...Object.values(datum.conflicts)[0]];
+              if(Object.values(datum.conflicts).length > 1) {
+                conflicts.push(...Object.values(datum.conflicts)[1])
+              }
               if (conflicts.includes(g2.name)) {
                 if (DEBUG) console.log(`conflict ${conflicts.toString()} has ${g2.name}`)
                 return false;
@@ -83,17 +92,22 @@ export const GET: APIRoute = async ({ params, request }) => {
         }
       });
       if (DEBUG) console.log(`found ${generalObjects.length} generals`);
-      const result = generalObjects.map((gi) => {
-        if (gi !== undefined && gi !== null) {
-          const name = gi.data.general.name;
-          return {
-            primary: id,
-            secondary: name,
-          };
-        }
-      });
-      if (DEBUG) console.log(`created ${result.length} results`)
-      return new Response(JSON.stringify(result));
+      
+      if(g1 !== undefined && g1 !== null ) {
+        const result = generalObjects.map((gi) => {
+          if ( gi !== undefined && gi !== null) {
+            const g2 = gi.data.general;
+             const returnable: GeneralPairType = {
+              primary: g1,
+              secondary: g2,
+            };
+            return returnable;
+          }
+          return null;
+        });
+        if (DEBUG) console.log(`created ${result.length} results`)
+        return new Response(JSON.stringify(result));
+      }
     }
   }
 
