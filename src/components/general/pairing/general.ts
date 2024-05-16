015db841e03487639531484f1d8f7f43d410eca1
Luke Stephen Schierer <luke@schierer.org>
2024-05-16 01:44:48 -0400

    still not working right locally, but I think I am getting closer

--- src/components/general/pairing/general.ts
+++ src/components/general/pairing/general.ts
@@ -15,6 +15,9 @@ import * as EvAnsRanking from "@lib/EvAnsAttributeRanking";
 
 import { z } from "astro:content";
 
+import { from, map, switchMap, throwError } from "rxjs";
+import {fromFetch} from 'rxjs/fetch'
+
 export const DEBUG = true;
 
 export class DisplayGeneral {
@@ -104,56 +107,77 @@ export class DisplayGeneral {
   }
 
   public async computeEvAnsRanking() {
+    let returnable = 0;
     if (this.general !== undefined) {
       let path = `generals/buffs/${this.general.name}.json`;
 
       if (DEBUG) console.log(`EvAnsUrl based on ${path}, ${this._url}`);
       const EvAnsUrl = new URL(path, this._url);
       if (DEBUG) console.log(`constructed url is ${EvAnsUrl.toString()}`);
-      const scoreData = fetch(EvAnsUrl)
-        .then((r) => {
-          if (r.ok) {
-            const v = z.array(BuffParams).safeParse( r.json());
-            if (v.success) {
-              const data: Array<BuffParamsType> = v.data;
-              const lessData = data.filter((datum) => {
-                if (this._dragon !== datum.dragon) {
-                  return false;
-                } else if (this._beast !== datum.beast) {
-                  return false;
-                } else if (this.stars.localeCompare(datum.stars)) {
-                  return false;
-                } else if (this._special1.localeCompare(datum.special1)) {
-                  return false;
-                } else if (this._special2.localeCompare(datum.special2)) {
-                  return false;
-                } else if (this._special3.localeCompare(datum.special3)) {
-                  return false;
-                } else if (this._special4.localeCompare(datum.special4)) {
-                  return false;
-                } else if (this._special5.localeCompare(datum.special5)) {
-                  return false;
-                }
-                return true;
-              });
-              if(lessData.length > 1) {
-                console.log(`found too many results in return, using first`)
-                return lessData[0].EvAnsRanking
-              } 
-              return lessData[0].EvAnsRanking;
+
+      const scoreObservable = fromFetch(EvAnsUrl.toString())
+        .pipe(switchMap((response) => {
+          if(!response.ok) {
+            console.log(`problem with fetch in computeEvAnsRanking() for ${this.general.name}`)
+            return throwError(() => new Error('bad response'))
+          }
+          return from(response.json())
+        }), map((response) => {
+          return z.array(BuffParams).parse(response)
+        }))
+
+        const lessData = new Array<BuffParamsType>();
+
+        scoreObservable.subscribe({
+          next: scoreData => {
+            lessData.push(...scoreData.filter((datum) => {
+              if (this._dragon !== datum.dragon) {
+                return false;
+              } else if (this._beast !== datum.beast) {
+                return false;
+              } else if (this.stars.localeCompare(datum.stars)) {
+                return false;
+              } else if (this._special1.localeCompare(datum.special1)) {
+                return false;
+              } else if (this._special2.localeCompare(datum.special2)) {
+                return false;
+              } else if (this._special3.localeCompare(datum.special3)) {
+                return false;
+              } else if (this._special4.localeCompare(datum.special4)) {
+                return false;
+              } else if (this._special5.localeCompare(datum.special5)) {
+                return false;
+              }
+              return true;
+            }));
+          },
+          error: error => {
+            if( error instanceof Error) {
+              console.log(error.message)
+            } else {
+              console.log(error)
             }
-          } else {
-            return 0;
+          },
+          complete: () => {
+            if(lessData.length > 1) {
+              console.log(`found too many results in return, using first anyway`)
+              returnable = lessData[0].EvAnsRanking;
+            } else if (lessData.length === 1) {
+              returnable = lessData[0].EvAnsRanking
+            } else {
+              returnable = -1;
+            }
           }
         })
         
-      if (DEBUG) console.log(`computeEvAnsRanking returning ${scoreData}`);
-      this._EvAnsRanking = Math.floor(1);
-      return scoreData;
+      if (DEBUG) console.log(`computeEvAnsRanking returning ${returnable}`);
+      this._EvAnsRanking = Math.floor(returnable);
+      return returnable;
     } else {
-      this._EvAnsRanking = 0;
-      return null;
+      returnable = -2;
     }
+    this._EvAnsRanking = returnable;
+    return returnable;
   }
 
   constructor(g: GeneralClassType, u: URL) {
