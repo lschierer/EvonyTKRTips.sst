ae5817a42e7fdc113265e1278cf79d2bdb1322f4
Luke Stephen Schierer <luke@schierer.org>
2024-05-27 17:13:19 -0400

    the error immediately moved to the next record lookup

--- src/middleware/DefenseRanking/ScoreComputer.ts
+++ src/middleware/DefenseRanking/ScoreComputer.ts
@@ -78,168 +78,6 @@ import { SiegePvPDefense } from "./Siege/PvPBase";
 
 export const DEBUG = false;
 
-const useCaseSelector: Record<
-  generalUseCaseType,
-  Record<
-    generalSpecialistsType,
-    (eg: ExtendedGeneralType, display: DisplayType, bp: BuffParamsType) => number
-  >
-> = {
-  [generalUseCase.enum.Attack]: {
-    [generalSpecialists.enum.Archers]: ArchersPvPDefense,
-    [generalSpecialists.enum.Ground]: GroundPvPDefense,
-    [generalSpecialists.enum.Mounted]: MountedPvPDefense,
-    [generalSpecialists.enum.Siege]: SiegePvPDefense,
-    [generalSpecialists.enum.Wall]: () => {
-      return -1;
-    },
-    [generalSpecialists.enum.Mayor]: () => {
-      return -2;
-    },
-    [generalSpecialists.enum.all]: () => {
-      return -3;
-    },
-  },
-  [generalUseCase.enum.Defense]: {
-    [generalSpecialists.enum.Archers]: () => {
-      return -7;
-    },
-    [generalSpecialists.enum.Ground]: () => {
-      return -7;
-    },
-    [generalSpecialists.enum.Mounted]: () => {
-      return -7;
-    },
-    [generalSpecialists.enum.Siege]: () => {
-      return -7;
-    },
-    [generalSpecialists.enum.Wall]: () => {
-      return -1;
-    },
-    [generalSpecialists.enum.Mayor]: () => {
-      return -2;
-    },
-    [generalSpecialists.enum.all]: () => {
-      return -3;
-    },
-  },
-  [generalUseCase.enum.Monsters]: {
-    [generalSpecialists.enum.Archers]: () => {
-      return -7;
-    },
-    [generalSpecialists.enum.Ground]: () => {
-      return -7;
-    },
-    [generalSpecialists.enum.Mounted]: () => {
-      return -7;
-    },
-    [generalSpecialists.enum.Siege]: () => {
-      return -7;
-    },
-    [generalSpecialists.enum.Wall]: () => {
-      return -1;
-    },
-    [generalSpecialists.enum.Mayor]: () => {
-      return -2;
-    },
-    [generalSpecialists.enum.all]: () => {
-      return -3;
-    },
-  },
-  [generalUseCase.enum.Overall]: {
-    [generalSpecialists.enum.Archers]: () => {
-      return -5;
-    },
-    [generalSpecialists.enum.Ground]: () => {
-      return -5;
-    },
-    [generalSpecialists.enum.Mounted]: () => {
-      return -5;
-    },
-    [generalSpecialists.enum.Siege]: () => {
-      return -5;
-    },
-    [generalSpecialists.enum.Wall]: () => {
-      return -5;
-    },
-    [generalSpecialists.enum.Mayor]: () => {
-      return -5;
-    },
-    [generalSpecialists.enum.all]: () => {
-      return -5;
-    },
-  },
-  [generalUseCase.enum.Wall]: {
-    [generalSpecialists.enum.Archers]: () => {
-      return -1;
-    },
-    [generalSpecialists.enum.Ground]: () => {
-      return -1;
-    },
-    [generalSpecialists.enum.Mounted]: () => {
-      return -1;
-    },
-    [generalSpecialists.enum.Siege]: () => {
-      return -1;
-    },
-    [generalSpecialists.enum.Wall]: () => {
-      return -1;
-    },
-    [generalSpecialists.enum.Mayor]: () => {
-      return -1;
-    },
-    [generalSpecialists.enum.all]: () => {
-      return -1;
-    },
-  },
-  [generalUseCase.enum.Mayor]: {
-    [generalSpecialists.enum.Archers]: () => {
-      return -2;
-    },
-    [generalSpecialists.enum.Ground]: () => {
-      return -2;
-    },
-    [generalSpecialists.enum.Mounted]: () => {
-      return -2;
-    },
-    [generalSpecialists.enum.Siege]: () => {
-      return -2;
-    },
-    [generalSpecialists.enum.Wall]: () => {
-      return -2;
-    },
-    [generalSpecialists.enum.Mayor]: () => {
-      return -2;
-    },
-    [generalSpecialists.enum.all]: () => {
-      return -2;
-    },
-  },
-  [generalUseCase.enum.all]: {
-    [generalSpecialists.enum.Archers]: () => {
-      return -3;
-    },
-    [generalSpecialists.enum.Ground]: () => {
-      return -3;
-    },
-    [generalSpecialists.enum.Mounted]: () => {
-      return -3;
-    },
-    [generalSpecialists.enum.Siege]: () => {
-      return -3;
-    },
-    [generalSpecialists.enum.Wall]: () => {
-      return -3;
-    },
-    [generalSpecialists.enum.Mayor]: () => {
-      return -3;
-    },
-    [generalSpecialists.enum.all]: () => {
-      return -3;
-    },
-  },
-};
-
 export const ScoreComputer = z
   .function()
   .args(generalUseCase, ExtendedGeneral, Display, BuffParams)
@@ -251,6 +89,27 @@ export const ScoreComputer = z
       display: DisplayType,
       bp: BuffParamsType
     ) => {
-      return useCaseSelector[UseCase][eg.general.score_as](eg, display, bp);
+      if(!UseCase.localeCompare(generalUseCase.enum.Attack)){
+        if(DEBUG) {
+          console.log(`called for Attack use case`)
+        }
+        if(generalSpecialists.enum.Archers.localeCompare(eg.general.score_as)){
+          return ArchersPvPDefense(eg, display, bp)
+        }
+        if(generalSpecialists.enum.Ground.localeCompare(eg.general.score_as)) {
+          return GroundPvPDefense(eg, display, bp)
+        }
+        if(generalSpecialists.enum.Mounted.localeCompare(eg.general.score_as)) {
+          return MountedPvPDefense(eg, display, bp)
+        }
+        if(generalSpecialists.enum.Siege.localeCompare(eg.general.score_as)) {
+          return SiegePvPDefense(eg, display, bp)
+        }
+        
+      } else {
+        console.log(`not called for Attack use case`)
+      }
+      console.log(`${eg.general.name} did not match any scoring function`)
+      return -7;
     }
   );
