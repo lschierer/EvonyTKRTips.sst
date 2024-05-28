a358bf3c0d0b7644d0854063745fa8f78ebaf05d
Luke Stephen Schierer <luke@schierer.org>
2024-05-28 02:27:50 -0400

    this likely *does* matter

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,18 +1,7 @@
 /// <reference path="../.astro/actions.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-/// <reference path="@schemas/EvAns.zod.ts" />
-/// <reference path="@schemas/ExtendedGeneral.ts" />
-/// <reference path="@schemas/baseSchemas.ts" />
-/// <reference path="@schemas/beastSchemas.ts" />
-/// <reference path="@schemas/blazonSchemas.ts" />
-/// <reference path="@schemas/bookSchemas.ts" />
-/// <reference path="@schemas/conflictSchemas.ts" />
-/// <reference path="@schemas/generalsSchema.ts" />
-/// <reference path="@schemas/specialitySchema.ts" />
-/// <reference path="@schemas/statusLightsSchema.ts" />
-/// <reference path="@schemas/treasureSchemas.ts" />
-/// <reference path="@schemas/troopsSchemas.ts" />
+
 
 declare namespace App {
   interface Locals {
