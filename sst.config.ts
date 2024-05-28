2f1e7633e125a190a11608ff2c4aadb711c25e23
Luke Stephen Schierer <luke@schierer.org>
2024-05-28 19:56:31 -0400

    I have decided to try sst ion at the same time, foolish as that may be, as I've noticed that's where all the support attention is focused

--- sst.config.ts
+++ sst.config.ts
@@ -1,16 +1,25 @@
-import { SSTConfig } from "sst";
-import { Web } from "./stacks/Web";
+/// <reference path="./.sst/platform/config.d.ts" />
 
-export default {
-  config(_input) {
+import { HostedZone } from "aws-cdk-lib/aws-route53";
+
+export default $config({
+  app(input) {
     return {
       name: "EvonyTKRTips",
       profile: "home",
-      region: "us-east-2",
+      buildCommand: "pnpm run build",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+      domain: {
+        name: input?.stage === 'production' ? "evonytkrtips.net" : `${input?.stage}.evonytkrtips.net`,
+        aliases: input?.stage === 'production' ? ["www.evonytkrtips.net"] : [`www.${input?.stage}.evonytkrtips.net`],
+        dns: sst.aws.dns({
+          zone: 'Z02705452UES0AYN9485J',
+        })
+      },
     };
   },
-  stacks(app) {
-    app
-      .stack(Web);
-  }
-} satisfies SSTConfig;
+  async run() {
+    new sst.aws.Astro("EvonyTKRTips");
+  },
+});
