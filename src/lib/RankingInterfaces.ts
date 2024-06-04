53021426cda5776f9cf1697b88b43a2583077b01
Luke Stephen Schierer <luke@schierer.org>
2024-06-04 18:13:24 -0400

    keep ranged here

--- src/lib/RankingInterfaces.ts
+++ src/lib/RankingInterfaces.ts
@@ -3,12 +3,13 @@ import type { BuffType, BuffParamsType } from '@schemas/baseSchemas';
 
 export interface BuffFunctionInterface {
   Attack: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType) => number,
-  MarchSize: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType ) => number,
-  HP: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType ) => number,
-  Defense: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType ) => number,
-  DeAttack: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType ) => number,
-  DeHP: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType ) => number,
-  DeDefense: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType ) => number,
-  Preservation: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType ) => number,
-  Debilitation: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType ) => number,
+  DeAttack: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType) => number,
+  DeDefense: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType) => number,
+  DeHP: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType) => number,
+  Debilitation: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType) => number,
+  Defense: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType) => number,
+  HP: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType) => number,
+  MarchSize: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType) => number,
+  Preservation: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType) => number,
+  Range: (name: string, buffName: string, actualBuff: BuffType, desired: BuffParamsType) => number,
 }
