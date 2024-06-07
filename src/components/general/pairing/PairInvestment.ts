095a16ee9b277ceba904e37a9189ff2fb8cd4f8b
Luke Stephen Schierer <luke@schierer.org>
2024-06-07 12:17:49 -0400

    start to think about this as a client compoent since the computations happen client side anyway

---
+++ src/components/general/pairing/PairInvestment.ts
@@ -0,0 +1,19 @@
+import { customElement, property, state } from 'lit/decorators.js';
+import { ref } from 'lit/directives/ref.js';
+
+import {
+  SizedMixin,
+  SpectrumElement,
+  type CSSResultArray,
+  html,
+  css,
+  unsafeCSS,
+  type PropertyValues,
+} from '@spectrum-web-components/base';
+
+import '@spectrum-css/tokens/dist/index.css';
+import '@spectrum-css/typography/dist/index.css';
+import '@spectrum-css/icon/dist/index.css';
+import '@spectrum-css/table/dist/index.css';
+
+
