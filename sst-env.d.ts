8fa0c37652079afe55cbca96bb6e3a3b709be49c
Luke Stephen Schierer <luke@schierer.org>
2024-06-16 13:45:58 -0400

    trying to make the linter happy looks like sst now uses this one

---
+++ sst-env.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+import "sst"
+declare module "sst" {
+  export interface Resource {
+    EvonyTKRTips: {
+      type: "sst.aws.Astro"
+      url: string
+    }
+  }
+}
+export {}
