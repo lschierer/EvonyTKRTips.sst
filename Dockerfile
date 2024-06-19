0ba9f5b5b093c9652b2d2de9963866ed37b1d07f
Luke Stephen Schierer <luke@schierer.org>
2024-06-19 11:30:02 -0400

    current progress towards finch/docker

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,24 @@
-FROM node:18-bullseye-slim
+FROM node:22-bullseye-slim AS base
 
+RUN apt update
+RUN apt install -y curl libcurl3-gnutls ca-certificates
+RUN npm i -g pnpm
+
+FROM base AS dependencies
 WORKDIR /app/
-
-COPY package.json /app
+COPY package.json pnpm-lock.yaml ./
 RUN pnpm install
 
-COPY dist /app
+FROM base AS build
+WORKDIR /app/
+COPY . .
+COPY --from=dependencies /app/node_modules ./node_modules
+RUN pnpm astro build
+RUN pnpm prune --prod
+
+FROM base AS deploy
+WORKDIR /app
+COPY --from=build /app/dist ./dist/
+COPY --from=build /app/node_modules ./node_modules
 
 ENTRYPOINT ["node", "dist/server/entry.mjs"]
--- sst.config.ts
+++ sst.config.ts
@@ -19,13 +19,26 @@ export default $config({
     };
   },
   async run() {
-    const EvonyVpc = new sst.aws.Vpc('EvonyVpc');
+    const EvonyVpc = new sst.aws.Vpc('EvonyVpc',{
+      transform: {
+        vpc: {
+          assignGeneratedIpv6CidrBlock: true,
+          enableDnsHostnames: true,
+          enableDnsSupport: true,
+          cidrBlock: "10.0.0.0/16",
+        }
+      }
+    });
 
     const EvonyCluster = new sst.aws.Cluster("EvonyTKRTips",{
       vpc: EvonyVpc,
     });
 
     EvonyCluster.addService('EvonyService', {
+      scaling: {
+        min: 1,
+        max: 2,
+      },
       public: {
         ports: [
           {
