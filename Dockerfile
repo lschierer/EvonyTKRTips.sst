58a7c416b0eaa6e54e961f58e442bb9031a0f7d0
Luke Stephen Schierer <luke@schierer.org>
2024-07-02 12:18:26 -0400

    feat: docker for astro
    I am not getting any response RE the astro-sst adapter.
    I really think I may need to invest the time for docker.

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,28 @@
-FROM node:22-bullseye-slim AS base
+FROM node:22-bookworm-slim AS base
+WORKDIR /app/
 
 RUN apt update
 RUN apt install -y curl libcurl3-gnutls ca-certificates
 RUN npm i -g pnpm
 
-FROM base AS dependencies
-WORKDIR /app/
 COPY package.json pnpm-lock.yaml ./
+
+FROM base AS prod-deps
+RUN pnpm install --prod
+
+FROM base AS build-deps
 RUN pnpm install
 
-FROM base AS build
-WORKDIR /app/
+FROM build-deps AS build
 COPY . .
-COPY --from=dependencies /app/node_modules ./node_modules
-RUN pnpm astro build
-RUN pnpm prune --prod
+RUN pnpm run build
 
-FROM base AS deploy
-WORKDIR /app
-COPY --from=build /app/dist ./dist/
-COPY --from=build /app/node_modules ./node_modules
+FROM base AS runtime
+COPY --from=prod-deps /app/node_modules ./node_modules
+COPY --from=build /app/dist ./dist
 
-ENTRYPOINT ["node", "dist/server/entry.mjs"]
+ENV HOST=0.0.0.0
+ENV PORT=4321
+EXPOSE 4321
+CMD node ./dist/server/entry.mjs
+
--- astro.config.ts
+++ astro.config.ts
@@ -1,26 +1,25 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightLinksValidator from 'starlight-links-validator';
-import lit from "@astrojs/lit";
+import lit from '@astrojs/lit';
 import { sidebar } from './src/sidebar.ts';
-import circleDependency from 'vite-plugin-circular-dependency'
+import circleDependency from 'vite-plugin-circular-dependency';
 
-import aws from "astro-sst";
-//import node from '@astrojs/node';
-
+//import aws from "astro-sst";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://evonytkrtips.net',
-  output: "hybrid",
-  adapter: aws({
+  output: 'hybrid',
+  /*  adapter: aws({
     //responseMode: "stream",
-  }),
-  /*adapter: node({
-    mode: 'standalone',
   }),*/
+  adapter: node({
+    mode: 'standalone',
+  }),
   devToolbar: {
-    enabled: false
+    enabled: false,
   },
   redirects: {
     '/generals/pair-picking/cavalry/': '/tools/pair-picking/mounted/',
@@ -32,78 +31,82 @@ export default defineConfig({
     defaultStrategy: 'tap',
   },
   compressHTML: false,
-  integrations: [lit(), starlight({
-    title: 'Evony TKR Tips',
-    disable404Route: true,
-    pagefind: false,
-    plugins: [starlightLinksValidator({
-      errorOnRelativeLinks: false
-    })],
-    components: {
-      PageFrame: './src/components/Spectrum/PageFrame.astro',
-      ThemeProvider: './src/components/Spectrum/ThemeProvider.astro',
-      TwoColumnContent: './src/components/TwoColumnContent.astro',
-      Sidebar: './src/components/Sidebar.astro'
+  integrations: [
+    lit(),
+    starlight({
+      title: 'Evony TKR Tips',
+      disable404Route: true,
+      pagefind: false,
+      plugins: [
+        starlightLinksValidator({
+          errorOnRelativeLinks: false,
+        }),
+      ],
+      components: {
+        PageFrame: './src/components/Spectrum/PageFrame.astro',
+        ThemeProvider: './src/components/Spectrum/ThemeProvider.astro',
+        TwoColumnContent: './src/components/TwoColumnContent.astro',
+        Sidebar: './src/components/Sidebar.astro',
+      },
+      customCss: [
+        '@spectrum-web-components/styles/typography.css',
+        '@spectrum-web-components/styles/scale-medium.css',
+        '@spectrum-css/tokens/dist/index.css',
+        '@spectrum-css/vars/dist/spectrum-global.css',
+        '@spectrum-css/vars/dist/spectrum-medium.css',
+        '@spectrum-css/page/dist/index.css',
+        'tabulator-tables/dist/css/tabulator.min.css',
+        './src/styles/custom.css',
+      ],
+      head: [
+        {
+          tag: 'script',
+          attrs: {
+            src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8360834774752607',
+            defer: true,
+          },
+        },
+        {
+          tag: 'meta',
+          attrs: {
+            name: 'google-adsense-account',
+            content: 'ca-pub-8360834774752607',
+          },
+        },
+      ],
+      lastUpdated: true,
+      pagination: true,
+      sidebar: sidebar,
+      social: {
+        github: 'https://github.com/lschierer/EvonyTKRTips.sst',
+      },
+    }),
+    {
+      name: 'sidebar-watch',
+      hooks: {
+        'astro:config:setup': ({ addWatchFile, config }) => {
+          addWatchFile(new URL('./src/sidebar.ts', config.root));
+        },
+      },
     },
-    customCss: [
-      '@spectrum-web-components/styles/typography.css',
-      '@spectrum-web-components/styles/scale-medium.css',
-      '@spectrum-css/tokens/dist/index.css',
-      '@spectrum-css/vars/dist/spectrum-global.css',
-      '@spectrum-css/vars/dist/spectrum-medium.css',
-      '@spectrum-css/page/dist/index.css',
-      'tabulator-tables/dist/css/tabulator.min.css',
-      './src/styles/custom.css'
-    ],
-    head: [{
-      tag: 'script',
-      attrs: {
-        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8360834774752607",
-        defer: true
-      }
-    }, {
-      tag: 'meta',
-      attrs: {
-        name: "google-adsense-account",
-        content: "ca-pub-8360834774752607"
-      }
-    }],
-    lastUpdated: true,
-    pagination: true,
-    sidebar: sidebar,
-    social: {
-      github: 'https://github.com/lschierer/EvonyTKRTips.sst'
-    }
-  }), {
-    name: "sidebar-watch",
-    hooks: {
-      "astro:config:setup": ({
-        addWatchFile,
-        config
-      }) => {
-        addWatchFile(new URL("./src/sidebar.ts", config.root));
-      }
-    }
-  }],
+  ],
   vite: {
     build: {
       minify: false,
-      cssMinify: false
+      cssMinify: false,
     },
     plugins: [
       circleDependency({
         outputFilePath: './circular-deps.txt',
         circleImportThrowErr: true,
-        include: [
-          /\.[jt]sx?$/, /\.astro\??/
-        ]
-      })
-    ]
+        include: [/\.[jt]sx?$/, /\.astro\??/],
+      }),
+    ],
   },
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
   image: {
     service: {
-      entrypoint: 'astro/assets/services/sharp'
-    }
-  }
+      entrypoint: 'astro/assets/services/sharp',
+    },
+  },
 });
