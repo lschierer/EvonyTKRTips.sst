9c793058eed32518a63d1347df2e453283d96a85
Luke Stephen Schierer <luke@schierer.org>
2024-07-03 17:42:22 -0400

    fix: a couple of basic attributes

--- src/EvonyPerl/lib/Game/Evony/GeneralPair.pm
+++ src/EvonyPerl/lib/Game/Evony/GeneralPair.pm
@@ -1,105 +1,44 @@
 package Game::Evony::GeneralPair;
 
-use 5.006;
+use 5.40.0;
 use strict;
 use warnings;
-
-=head1 NAME
-
-Game::Evony::GeneralPair - The great new Game::Evony::GeneralPair!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+use Moo;
+use Types::Standard qw( Str Int Enum ArrayRef InstanceOf );
+use Type::Params qw( signature );
+use namespace::autoclean;
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Game::Evony::GeneralPair;
-
-    my $foo = Game::Evony::GeneralPair->new();
-    ...
+In Evony, most generals are most usefully evaluated as part of a pair,
+not as a stand-alone general.
 
 =head1 EXPORT
 
 A list of functions that can be exported.  You can delete this section
 if you don't export anything, such as for a purely object-oriented module.
 
-=head1 SUBROUTINES/METHODS
+=attr primary
 
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
+The primary general of the pair
 
 =cut
 
-sub function2 {
-}
+has 'primary' => (
+  is  => 'rw',
+  isa => InstanceOf[ 'Games::Evony::General' ],
+);
 
-=head1 AUTHOR
+=attr secondary
 
- Luke Schierer, C<< < lschiere at amazon.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-. at rt.cpan.org>, or through
-the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=.>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Game::Evony::GeneralPair
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=.>
-
-=item * CPAN Ratings
-
-L<https://cpanratings.perl.org/d/.>
-
-=item * Search CPAN
-
-L<https://metacpan.org/release/.>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-This software is Copyright (c) 2024 by  Luke Schierer.
-
-This is free software, licensed under:
-
-  The Artistic License 2.0 (GPL Compatible)
-
+The secondary general of the pair
 
 =cut
+
+has 'secondary' => (
+  is  => 'rw',
+  isa => InstanceOf[ 'Games::Evony::General' ],
+);
+
 
 1; # End of Game::Evony::GeneralPair
