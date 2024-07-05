b1049564e68a5467ac8c56e3097cf055f2570999
Luke Stephen Schierer <luke@schierer.org>
2024-07-05 15:51:15 -0400

    feat: perl scaffoloding
    this version builds, though it does not actually include everything correctly yet

---
+++ src/EvonyPerl/lib/Game/Evony/Evony.pm
@@ -0,0 +1,20 @@
+use 5.40.0;
+use strict;
+use warnings;
+package Game::Evony;
+
+# ABSTRACT: Analysis tools for players of Evony TKR
+=head1 SYNOPSIS
+
+These modules is to assist players of the online game _Evony: The King's Return_.
+It includes methods for reading in data files describing Evony Generals,
+creating tables comparing the performance of those generals (usually as
+pairs), and hopefully more over time.
+
+
+This top level Game::Evony module exists primary as a placeholder to
+organize the underlying modules in the namespace. It does not really
+do anything on its own.
+=cut
+
+1;
