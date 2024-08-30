7fc26e59a15ff79b535b352fa59afe5f5e277c72
Luke Schierer <lschiere@amazon.com>
2024-08-30 18:29:36 -0400

    merge from astro_starlight branch for this file

--- sst.config.ts
+++ sst.config.ts
@@ -1,29 +1,72 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
-import { HostedZone } from "aws-cdk-lib/aws-route53";
-
-
 export default $config({
   app(input) {
     return {
       name: "evonytkrtips",
-      profile: "home",
-      buildCommand: "pnpm run build",
-      removal: input?.stage === "production" ? "retain" : "remove",
+      removal: input?.stage.toLocaleLowerCase().includes("prod")
+        ? "retain"
+        : "remove",
       home: "aws",
       providers: {
         aws: {
-          region: "us-east-2"
+          profile: "home",
+          region: 'us-east-2',
+        },
+      },
+      console: {
+        autodeploy: {
+          target(event) {
+            if (event.type === "branch" && event.branch === "production" && event.action === "pushed") {
+              return {
+                stage: "production",
+                runner: { engine: "codebuild", compute: "small" }
+              };
+            } else if (event.type === "branch" && event.branch === "astro_starlight" && event.action === "pushed") {
+              return {
+                stage: "dev",
+                runner: { engine: "codebuild", compute: "small" }
+              };
+            } else if(event.action === "pushed") {
+              return {
+                stage: event.branch,
+                runner: { engine: "codebuild", compute: "small" }
+              };
+            }
+          }
         }
       },
     };
   },
   async run() {
-    new sst.aws.Astro("EvonyTKRTips",{
+    const site = new sst.aws.Astro("Site", {
+      path: './',
+      buildCommand: "pnpm build",
+      dev: {
+        command: "pnpm dev",
+        url: "http://localhost:4321/",
+      },
+      server: {
+        memory: "4 GB",
+      },
+      transform: {
+        cdn: {
+          defaultRootObject: "index.html",
+        }
+      },
       domain: {
-        name: $app.stage === 'production' ? "evonytkrtips.net" : `${$app?.stage}.evonytkrtips.net`,
-        aliases: $app?.stage === 'production' ? ["www.evonytkrtips.net"] : [`www.${$app?.stage}.evonytkrtips.net`],
+        name: $app.stage.toLocaleLowerCase().includes("prod") ? 
+          "evonytkrtips.net" : 
+          `${$app.stage.toLocaleLowerCase()}.evonytkrtips.net`,
+        aliases: [
+            $app.stage.toLocaleLowerCase().includes("prod") ? 
+          "www.evonytkrtips.net" : 
+          `www.${$app.stage.toLocaleLowerCase()}.evonytkrtips.net`,
+        ],
+        dns: sst.aws.dns({
+            
+          }),
       },
     });
+    
   },
 });
